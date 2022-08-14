87ab159948477b73c4b198c1029e519d2e3dac00
arbuzaicer <maxpachinov@gmail.com>
2022-08-14 19:10:33 +0300

    fix: fixed faq page

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,14 +5,15 @@ import "./Header.scss";
 import "aos/dist/aos.css";
 
 const Header = () => {
-
-  useEffect(() => { AOS.init({ duration: 1000 }); }, []);
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
 
   function toggle() {
     const toggleButton = document.getElementById("toggle");
-    const togglelinks = document.getElementById('links');
+    const togglelinks = document.getElementById("links");
     toggleButton.classList.toggle("active");
-    togglelinks.classList.toggle('active');
+    togglelinks.classList.toggle("active");
   }
 
   return (
@@ -22,52 +23,89 @@ const Header = () => {
         <div className="content-menu">
           <ul className="links" id="links">
             <li className="links-li">
-              <a className="desktop-link font-custom-3" href="/">Главная</a>
+              <a className="desktop-link font-custom-3" href="/">
+                Главная
+              </a>
               <input type="checkbox" id="one-menu" />
-              <label for="one-menu font-custom-3">Главная</label>
+              <label htmlFor="one-menu font-custom-3">Главная</label>
             </li>
             <li className="links-li">
-              <a className="desktop-link font-custom-3" href="/">Полезное</a>
+              <a className="desktop-link font-custom-3" href="/">
+                Полезное
+              </a>
               <input type="checkbox" id="two-menu" />
-              <label for="two-menu font-custom-3">Полезное</label>
+              <label htmlFor="two-menu font-custom-3">Полезное</label>
               <ul className="drop">
-                <li className="drop-li"><a href="/">Вики</a></li>
-                <li className="drop-li"><a href="/">Моды</a></li>
-                <li className="drop-li"><a href="/">Онлайн карты</a></li>
-                <li className="drop-li"><a href="/">Статистика</a></li>
-                <li className="drop-li"><a href="/">Поддержать</a></li>
+                <li className="drop-li">
+                  <a href="/">Вики</a>
+                </li>
+                <li className="drop-li">
+                  <a href="/">Моды</a>
+                </li>
+                <li className="drop-li">
+                  <a href="/">Онлайн карты</a>
+                </li>
+                <li className="drop-li">
+                  <a href="/">Статистика</a>
+                </li>
+                <li className="drop-li">
+                  <a href="/">Поддержать</a>
+                </li>
               </ul>
             </li>
             <li className="links-li">
-              <a className="desktop-link font-custom-3" href="/">Правила</a>
+              <a className="desktop-link font-custom-3" href="/">
+                Правила
+              </a>
               <input type="checkbox" id="three-menu" />
-              <label for="three-menu font-custom-3">Правила</label>
+              <label htmlFor="three-menu font-custom-3">Правила</label>
             </li>
             <li className="links-li">
-              <a className="desktop-link font-custom-3" href="/faq">faq</a>
+              <a className="desktop-link font-custom-3" href="/faq">
+                faq
+              </a>
               <input type="checkbox" id="four-menu" />
-              <label for="four-menu font-custom-3">faq</label>
+              <label htmlFor="four-menu font-custom-3">faq</label>
             </li>
             <li className="links-li">
-              <a className="desktop-link font-custom-3" href="/cab">Профиль</a>
+              <a className="desktop-link font-custom-3" href="/cab">
+                Профиль
+              </a>
               <input type="checkbox" id="five-menu" />
-              <label for="five-menu font-custom-3">Профиль</label>
+              <label htmlFor="five-menu font-custom-3">Профиль</label>
               <ul className="drop">
                 <li className="drop-li ico-pl-li">
-                  <img className="ico-player" src="https://minotar.net/avatar/prestig9110/30" alt="none"></img>
-                  <a href="/cab" className="ico-name-player font-custom-2">prestig9110</a></li>
-                <li className="drop-li"><a href="/cab">Профиль</a></li>
-                <li className="drop-li"><a href="/cab">Мои территории</a></li>
-                <li className="drop-li"><a href="/cab">Мои метки</a></li>
-                <li className="drop-li"><a href="/cab">Статьи</a></li>
-                <li className="drop-li"><a href="/cab">Призы</a></li>
+                  <img
+                    className="ico-player"
+                    src="https://minotar.net/avatar/prestig9110/30"
+                    alt="none"
+                  ></img>
+                  <a href="/cab" className="ico-name-player font-custom-2">
+                    prestig9110
+                  </a>
+                </li>
+                <li className="drop-li">
+                  <a href="/cab">Профиль</a>
+                </li>
+                <li className="drop-li">
+                  <a href="/cab">Мои территории</a>
+                </li>
+                <li className="drop-li">
+                  <a href="/cab">Мои метки</a>
+                </li>
+                <li className="drop-li">
+                  <a href="/cab">Статьи</a>
+                </li>
+                <li className="drop-li">
+                  <a href="/cab">Призы</a>
+                </li>
               </ul>
             </li>
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Header;
