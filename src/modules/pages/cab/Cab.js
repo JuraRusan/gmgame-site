977cdbe1eb1f7b2eff4065ce9311ca9338d86a49
AndreyV <varganov.a.p@gmail.com>
2022-08-15 18:30:48 +0300

    переделал ЛК на react-tabs

--- src/modules/pages/cab/Cab.js
+++ src/modules/pages/cab/Cab.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AOS from "aos";
 import useAxios from '../../../DataProvider';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import "./Cab.scss";
 import "aos/dist/aos.css";
@@ -16,7 +17,9 @@ import MyPrizes from "../../components/my-prizes/My-prizes.js";
 import ChangePassword from "../../components/change-password/Change-password.js";
 
 
-const Cab = () => {
+const Cab = (props) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   const { cancel, data, error, loaded } = useAxios(
     "/api/me/",
     'GET',
@@ -37,32 +40,63 @@ const Cab = () => {
     return (
       <div className="main-cab" data-aos="fade-up">
         <Header />
+        <Tabs
+          selectedIndex={selectedIndex}
+          onSelect={(selectedIndex) => setSelectedIndex(selectedIndex)}
+          selectedTabClassName="checked"
+          // selectedTabPanelClassName="avengers-tab-panel--selected"
+        >
         <div className="box">
+        <TabList>
           <div className="col-1">
             <PlayerCabinet {...data.user} />
             <div className="menu-cabinet">
               <div className="m1">
-                <label className="tab checked">{profileMenuMyProfile}</label>
-                <label className="tab">{profileMenuMyTerritories}</label>
-                <label className="tab">{profileMenuMyMarker}</label>
-                <label className="tab">{profileMenuMyArticles}</label>
-                <label className="tab">{profileMenuMyPrizes}</label>
+                <Tab className="tab">{profileMenuMyProfile}</Tab>
+                <Tab className="tab">{profileMenuMyTerritories}</Tab>
+                <Tab className="tab">{profileMenuMyMarker}</Tab>
+                <Tab className="tab">{profileMenuMyArticles}</Tab>
+                <Tab className="tab">{profileMenuMyPrizes}</Tab>
+                <Tab className="tab">{profileMenuMyChangePassword}</Tab>
               </div>
               <div className="m1">
-                <label className="tab">{profileMenuMyChangePassword}</label>
                 <label className="tab">{profileMenuMyGoOut}</label>
               </div>
             </div>
           </div>
-          <div className="col-2">
-            <MyProfile userDC={data.discordUser} user={data.user} version={data.version} />
-            {/* <MyTerritories /> */}
-            {/* <MyMarkers /> */}
-            {/* <Articles /> */}
-            {/* <MyPrizes /> */}
-            {/* <ChangePassword /> */}
-          </div>
+          </TabList>
+            <TabPanel>
+              <div className="col-2">
+                <MyProfile userDC={data.discordUser} user={data.user} version={data.version} />
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="col-2">
+                <MyTerritories />
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="col-2">
+                <MyMarkers />
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="col-2">
+                <Articles />
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="col-2">
+                <MyPrizes />
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="col-2">
+                <ChangePassword />
+              </div>
+            </TabPanel>
         </div>
+        </Tabs>
         <Fotter />
       </div>
     );
