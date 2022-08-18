8d4c44ca17b501f95b887106a2c7e9b8feae8a5c
jurarusan <rusanjura@gmail.com>
2022-08-18 11:20:03 +0300

    призы всё + огромнейшая база майнкрафт пнж

--- src/modules/components/my-prizes/My-prizes.js
+++ src/modules/components/my-prizes/My-prizes.js
@@ -8,14 +8,83 @@ const MyPrizes = () => {
   return (
     <div className="box-prizes">
       <h4 className="prizes-h4 font-custom-2">У Вас 100500 выигрышей</h4>
-      <div className="one-prizes">
-        <div className="img-block">
-          <div className="animation-bg"></div>
+      <div className="all-prizes">
+        <div className="one-prizes">
+          <div className="img-block">
+            <div className="animation-bg-item"></div>
+          </div>
+          <div className="prizes-all">
+            <h5 className="text-h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet non omnis
+              provident quaerat quibusdam quos rem tempora, voluptas. Aperiam dolorum enim fugit in magnam nisi quisquam
+              vel veritatis voluptas!</h5>
+            <button className="pick">Забрать</button>
+          </div>
         </div>
-        <div className="prizes-all">
-          <h5 className="text-h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet non omnis provident quaerat quibusdam
-            quos rem tempora, voluptas. Aperiam dolorum enim fugit in magnam nisi quisquam vel veritatis voluptas!</h5>
-          <button className="pick">Забрать</button>
+        <div className="one-prizes">
+          <div className="img-block">
+            <div className="animation-bg-money"></div>
+          </div>
+          <div className="prizes-all">
+            <h5 className="text-h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet non omnis
+              provident quaerat quibusdam quos rem tempora, voluptas. Aperiam dolorum enim fugit in magnam nisi quisquam
+              vel veritatis voluptas!</h5>
+            <button className="pick">Забрать</button>
+          </div>
+        </div>
+        <div className="one-prizes">
+          <div className="img-block">
+            <div className="animation-bg-item"></div>
+          </div>
+          <div className="prizes-all">
+            <h5 className="text-h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet non omnis
+              provident quaerat quibusdam quos rem tempora, voluptas. Aperiam dolorum enim fugit in magnam nisi quisquam
+              vel veritatis voluptas!</h5>
+            <button className="pick">Забрать</button>
+          </div>
+        </div>
+        <div className="one-prizes">
+          <div className="img-block">
+            <div className="animation-bg-money"></div>
+          </div>
+          <div className="prizes-all">
+            <h5 className="text-h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet non omnis
+              provident quaerat quibusdam quos rem tempora, voluptas. Aperiam dolorum enim fugit in magnam nisi quisquam
+              vel veritatis voluptas!</h5>
+            <button className="pick">Забрать</button>
+          </div>
+        </div>
+        <div className="one-prizes">
+          <div className="img-block">
+            <div className="animation-bg-item"></div>
+          </div>
+          <div className="prizes-all">
+            <h5 className="text-h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet non omnis
+              provident quaerat quibusdam quos rem tempora, voluptas. Aperiam dolorum enim fugit in magnam nisi quisquam
+              vel veritatis voluptas!</h5>
+            <button className="pick">Забрать</button>
+          </div>
+        </div>
+        <div className="one-prizes">
+          <div className="img-block">
+            <div className="animation-bg-money"></div>
+          </div>
+          <div className="prizes-all">
+            <h5 className="text-h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet non omnis
+              provident quaerat quibusdam quos rem tempora, voluptas. Aperiam dolorum enim fugit in magnam nisi quisquam
+              vel veritatis voluptas!</h5>
+            <button className="pick">Забрать</button>
+          </div>
+        </div>
+        <div className="one-prizes">
+          <div className="img-block">
+            <div className="animation-bg-item"></div>
+          </div>
+          <div className="prizes-all">
+            <h5 className="text-h5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet non omnis
+              provident quaerat quibusdam quos rem tempora, voluptas. Aperiam dolorum enim fugit in magnam nisi quisquam
+              vel veritatis voluptas!</h5>
+            <button className="pick">Забрать</button>
+          </div>
         </div>
       </div>
     </div>
