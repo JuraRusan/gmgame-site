79238e6ae497204435d33259ffc346ecbf2725e5
jurarusan <rusanjura@gmail.com>
2023-01-12 23:00:43 +0200

    переделка страницы супорт + добавление суперхаба

--- src/modules/pages/support/Support.js
+++ src/modules/pages/support/Support.js
@@ -3,6 +3,8 @@ import AOS from "aos";
 
 import "./Support.scss";
 import "aos/dist/aos.css";
+import Image7 from "../../../bases/assets/7.png";
+import SuperHub from "../../../bases/assets/SuperHub.webp";
 
 const Support = () => {
 
@@ -14,15 +16,24 @@ const Support = () => {
     <div className="main-support" data-aos="fade-up">
       <div className="support">
         <h4 className="title-support">Поддержать проект</h4>
-        <h5 className="sub-title-support">Сервер GMGame является абсолютно бесплатным. Он существует и развивается
-          только за счет активных игроков,
-          ламповой атмосферы и ваших пожертвований.</h5>
-        <a target="_blank" className="support-link" href="https://new.donatepay.ru/@gmgame">- Донатом &#129133;</a>
-        <a target="_blank" className="support-link" href="https://hotmc.ru/casino-205185">- Розыгрыш на мониторинге (HotMC) &#129133;</a>
-        <a target="_blank" className="support-link" href="https://discord.gg/AesVsdPsFj">- Проголосовать на мониторингах &#129133;</a>
-        <h5 className="footer-support">Спасибо! Ваша поддержка очень важна. &#129392;</h5>
+        <div className="don-block">
+          <div className="text">
+            <p className="sub-title-support">Сервер GMGame работает уже больше двух лет. За это время на сервере были построены разнообразные и и масштабные проекты, проведены различные ивенты и праздники, сервер приобрел множество друзей, соратников и подарил множество эмоций разным людям.</p>
+            <p className="sub-title-support">Благодаря этим людям сервер живет и позволяет комфортно играть без лагов и читеров. Мы стараемся каждый день улучшить игровой опыт на сервере и будем признательны за любую помощь, если у вас есть возможность помогать серверу любым способом, пожалуйста, воспользуйтесь ей.</p>
+            <p className="sub-title-support">На пожертвования уже куплено мощное железо для сервера, источник бесперебойного питания и множество плагинов для комфортной игры. Спасибо всем игрокам, без вашей поддержки не было бы сервера GMGame.</p>
+            <div className="link">
+              <a target="_blank" className="support-link" href="https://new.donatepay.ru/@gmgame">- Донатом &#129133;</a>
+              <a target="_blank" className="support-link" href="https://hotmc.ru/casino-205185">- Розыгрыш на мониторинге (HotMC) &#129133;</a>
+              <a target="_blank" className="support-link" href="https://discord.gg/AesVsdPsFj">- Проголосовать на мониторингах &#129133;</a>
+            </div>
+            <h5 className="footer-support">Спасибо! Ваша поддержка очень важна. &#129392;</h5>
+          </div>
+          <div className="img-block-wight">
+            <img className='info-img-wight' src={Image7} alt=""/>
+          </div>
+        </div>
         <div className="monitoring-block">
-          <h4 className="monitoring-title">Мы на мониторингах:</h4>
+          <h4 className="block-title-sp">Мы на мониторингах</h4>
           <a className="monitoring-card font-custom-2" target="_blank" href="https://hotmc.ru/minecraft-server-205185">HotMC</a>
           <a className="monitoring-card font-custom-2" target="_blank" href="https://mineserv.top/gmgame">MineServ</a>
           <a className="monitoring-card font-custom-2" target="_blank" href="https://monitoringminecraft.ru/server/694954">Monitoring minecraft</a>
@@ -35,6 +46,19 @@ const Support = () => {
           <a className="monitoring-card font-custom-2" target="_blank" href="https://klauncher.ru/monitoring/server/2316">Klauncher</a>
           <a className="monitoring-card font-custom-2" target="_blank" href="https://serversminecraft.ru/server/944">Serversminecraft</a>
           <a className="monitoring-card font-custom-2" target="_blank" href="https://top.grmc.su/server/2508">Top.grmc</a>
+        </div>
+        <div className="partner-block">
+          <h4 className="block-title-sp">Наши партнеры</h4>
+          <div className="superHub">
+            <div className="logo-line-1">
+              <img className="superhub-logo" src={SuperHub} alt=""/>
+            </div>
+            <div className="logo-line-2">
+              <h4 className="name font-custom-2">Hosting Superhub</h4>
+              <h5 className="sub-name">Дешёвый и надёжный хостинг для Minecraft серверов.</h5>
+              <a className="support-link font-custom-2" target="_blank" href="https://superhub.host/referral/c45b819b">Hosting Superhub &#129133;</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
