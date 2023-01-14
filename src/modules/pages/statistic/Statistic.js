54b4129874d3a1212dc2e8bdffad939266d8ea4e
dalandis <varganov.a.p@gmail.com>
2023-01-14 13:58:31 +0500

    переделана статистика

--- src/modules/pages/statistic/Statistic.js
+++ src/modules/pages/statistic/Statistic.js
@@ -1,21 +1,38 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useMemo} from "react";
 import AOS from "aos";
 import {useAxios} from '../../../DataProvider';
-import ReactDataGrid from '@inovua/reactdatagrid-community'
-import '@inovua/reactdatagrid-community/index.css'
 import Preload from "../../components/preloader/Preload";
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 import "./Statistic.scss";
 import "aos/dist/aos.css";
 
 const Statistic = () => {
-  const [limit, setLimit] = useState(15);
+  const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
+  const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
 
   const resParams = useAxios(
     "/api/get_statistics",
     'GET',
     {}
   );
+
+  const numberSort = (num1, num2) => {
+    return num1 - num2;
+  };
+
+  const [columns, setColumnDefs] = useState([
+    { headerName: 'Ник', field: 'name', filter: true, sortable: true },
+    { headerName: 'Активное время игры', field: 'active_playtime', sortable: true, valueFormatter: params => timeFormat(params), comparator: numberSort},
+    { headerName: 'АФК', field: 'afk', sortable: true,  valueFormatter: params => timeFormat(params), comparator: numberSort},
+    { headerName: 'Смерти', field: 'deaths', sortable: true, comparator: numberSort },
+    { headerName: 'Убито мобов', field: 'mobs', sortable: true, comparator: numberSort },
+    { headerName: 'Сломано блоков', field: 'broken', sortable: true, comparator: numberSort, valueFormatter: ({value}) => !value ? 0 : value },
+    { headerName: 'Установлено блоков', field: 'supplied', sortable: true, comparator: numberSort, valueFormatter: ({value}) => !value ? 0 : value },
+  ]);
 
   useEffect(() => {
     AOS.init({duration: 1000});
@@ -25,7 +42,8 @@ const Statistic = () => {
     return <Preload/>;
   }
 
-  function timeFormat(value) {
+  function timeFormat(params) {
+    const value = params.value;
     const cd = 24 * 60 * 60 * 1000;
     const ch = 60 * 60 * 1000;
 
@@ -57,16 +75,6 @@ const Statistic = () => {
 
     return d + "д " + pad(h) + "ч " + pad(m) + "м";
   }
-
-  const columns = [
-    { name: 'name', header: 'name', minWidth: 50 },
-    { name: 'active_playtime', header: 'active_playtime', maxWidth: 1000, type: 'number', render: ({ value }) => timeFormat(value), defaultFlex: 1 },
-    { name: 'afk', header: 'afk', minWidth: 50, type: 'number', render: ({ value }) => timeFormat(value), defaultFlex: 1},
-    { name: 'deaths', header: 'deaths', maxWidth: 1000, type: 'number', defaultFlex: 1 },
-    { name: 'mobs', header: 'mobs', minWidth: 50, type: 'number', defaultFlex: 1 },
-    { name: 'broken', header: 'broken', maxWidth: 1000, type: 'number', render: ({value}) => !value ? 0 : value, defaultFlex: 1 },
-    { name: 'supplied', header: 'supplied', minWidth: 50, type: 'number', defaultFlex: 1 },
-  ];
 
   const data = [
     {
@@ -1824,28 +1832,126 @@ const Statistic = () => {
         "broken": null,
         "supplied": null
     }
-]
+  ]
 
-const filterValue = [
-  { name: 'name', operator: 'contains', type: 'string', value: '' }
-];
+  const localText = {
+    // for filter panel
+    page: 'Страница',
+    more: 'ещё',
+    to: 'к',
+    of: 'из',
+    next: 'Следующая',
+    last: 'Последняя',
+    first: 'Первая',
+    previous: 'Предыдущая',
+    loadingOoo: 'Загрузка...',
 
-  const gridStyle = { minHeight: 550 };
+    // for set filter
+    selectAll: 'Выделить всё',
+    searchOoo: 'Поиск...',
+    blanks: 'Ничего не найдено',
+
+    // for number filter and text filter
+    filterOoo: 'Фильтровать...',
+    applyFilter: 'Применить фильтр...',
+    equals: 'Равно',
+    notEqual: 'Не равно',
+
+    // for number filter
+    lessThan: 'Меньше чем',
+    greaterThan: 'Больше чем',
+    lessThanOrEqual: 'Меньше или равно',
+    greaterThanOrEqual: 'Больше или равно',
+    inRange: 'В промежутке',
+
+    // for text filter
+    contains: 'Содержит',
+    notContains: 'Не содержит',
+    startsWith: 'Начинается с',
+    endsWith: 'Заканчивается',
+
+    // filter conditions
+    andCondition: '"И"',
+    orCondition: '"ИЛИ"',
+
+    // the header of the default group column
+    group: 'Группа',
+
+    // tool panel
+    columns: 'Столбцы',
+    filters: 'Фильтры',
+    rowGroupColumns: 'Столбцы группировки по строкам',
+    rowGroupColumnsEmptyMessage: 'Перетащите сюда для группировки по строкам',
+    valueColumns: 'Столбцы со значениями',
+    pivotMode: 'Режим сводной таблицы',
+    groups: 'Группы',
+    values: 'Значения',
+    pivots: 'Заголовки столбцов',
+    valueColumnsEmptyMessage: 'Перетащите сюда для агрегации',
+    pivotColumnsEmptyMessage: 'Перетащите сюда, чтобы задать заголовки столбам',
+    toolPanelButton: 'Панель инструментов',
+
+    // other
+    noRowsToShow: 'Нет данных',
+
+    // enterprise menu
+    pinColumn: 'Закрепить колонку',
+    valueAggregation: 'Агрегация по значению',
+    autosizeThiscolumn: 'Автоматически задавать размер этой колонки',
+    autosizeAllColumns: 'Автоматически задавать размер всем колонкам',
+    groupBy: 'Группировать по',
+    ungroupBy: 'Разгруппировать по',
+    resetColumns: 'Сбросить столбцы',
+    expandAll: 'Развернуть всё',
+    collapseAll: 'Свернуть всё',
+    toolPanel: 'Панель инструментов',
+    export: 'Экспорт',
+    csvExport: 'Экспорт в CSV',
+    excelExport: 'Экспорт в Excel (.xlsx)',
+    excelXmlExport: 'Экспорт в XML (.xml)',
+
+    // enterprise menu pinning
+    pinLeft: 'Закрепить слева <<',
+    pinRight: 'Закрепить справа >>',
+    noPin: 'Не закреплять <>',
+
+    // enterprise menu aggregation and status bar
+    sum: 'Сумма',
+    min: 'Минимум',
+    max: 'Максимум',
+    none: 'Пусто',
+    count: 'Количество',
+    average: 'Среднее значение',
+    filteredRows: 'Отфильтрованные',
+    selectedRows: 'Выделенные',
+    totalRows: 'Всего строк',
+    totalAndFilteredRows: 'Строк',
+
+    // standard menu
+    copy: 'Копировать',
+    copyWithHeaders: 'Копировать с заголовком',
+    ctrlC: 'Ctrl+C',
+    paste: 'Вставить',
+    ctrlV: 'Ctrl+V'
+}
 
   return (
     <div className="main-statistic" data-aos="fade-up">
       <div className="statistic">
         <h4>ух какая пиздатая статка</h4>
-        <ReactDataGrid
-          idProperty="name"
-          columns={columns}
-          dataSource={data} //resParams.data
-          style={gridStyle}
-          pagination="local"
-          limit={limit}
-          onLimitChange={setLimit}
-          defaultFilterValue={filterValue}
-        />
+        <div style={containerStyle}>
+          <div style={gridStyle} className="ag-theme-alpine">
+            <AgGridReact
+              rowData={data} // resParams.data
+              columnDefs={columns}
+              animateRows={true}
+              pagination={true}
+              paginationPageSize={15}
+              localeText={localText}
+            >
+            </AgGridReact>
+          </div>
+        </div>
       </div>
     </div>
   );
