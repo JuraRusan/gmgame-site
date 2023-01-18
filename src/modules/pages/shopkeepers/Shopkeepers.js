ba8ff1f8fc2639d45a9ce5a636367eb8220fb0df
jurarusan <rusanjura@gmail.com>
2023-01-18 19:09:08 +0200

    Еще немного стилей насыпал к магазинам

--- src/modules/pages/shopkeepers/Shopkeepers.js
+++ src/modules/pages/shopkeepers/Shopkeepers.js
@@ -12,8 +12,57 @@ const Shopkeepers = () => {
       <div className="shop-block-center">
         <div className="shop-all-suggestions">
           <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
+          <OneSuggestions/>
         </div>
-        <div className="shop-one-suggestions"><ShulkerBox/></div>
+        <div className="shop-one-suggestions">
+          <div className="prew-shulker">
+            <ShulkerBox/>
+          </div>
+          <div className="villager-shop-info">
+            <div className="villager-info">
+              <h4 className="h4-style">Владелец: <span className="span-custom-color font-custom-2">SoftPanda3</span></h4>
+              <h4 className="h4-style">Расположение:</h4>
+              <h4 className="h4-style h4-margin-left">Х: <span className="span-custom-color font-custom-2">12345</span></h4>
+              <h4 className="h4-style h4-margin-left">Y: <span className="span-custom-color font-custom-2">12345</span></h4>
+              <h4 className="h4-style h4-margin-left">Z: <span className="span-custom-color font-custom-2">12345</span></h4>
+              <h4 className="h4-style">Осталось предметов в наличии: <span className="span-custom-color font-custom-2">12345</span></h4>
+            </div>
+            <div className="villager-img">
+              <h2 className="villager-nameplate">Кузнечный магазин</h2>
+              <img className="margin-top-img" src="./site_assets/villager/webp/Savanna_Fisherman.webp" width="100%" height="100%" alt="none"/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
