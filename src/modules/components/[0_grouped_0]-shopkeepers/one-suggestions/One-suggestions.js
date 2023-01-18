de8df36a382db44c7ab72d7700cc301897d86b7c
dalandis <varganov.a.p@gmail.com>
2023-01-18 22:49:33 +0500

    магазины

--- src/modules/components/[0_grouped_0]-shopkeepers/one-suggestions/One-suggestions.js
+++ src/modules/components/[0_grouped_0]-shopkeepers/one-suggestions/One-suggestions.js
@@ -3,10 +3,10 @@ import React from "react";
 import "./One-suggestions.scss"
 import OneItem from "../one-item/One-item";
 
-const OneSuggestions = () => {
+const OneSuggestions = (props) => {
   return (
     <div className="one-suggestions-block">
-      <OneItem id="shulker-box-item-position-00" item_name_title="Алмазоносный глубинный сланец" item_minecraft_name_id="deepslate_diamond_ore" item_count="12"/>
+      <OneItem id="shulker-box-item-position-00" item_name_title="Алмазоносный глубинный сланец" item_minecraft_name_id={props.currency} item_count={props.amount}/>
       <span className="arrow-suggestions">&#10132;</span>
       <OneItem id="shulker-box-item-position-00" item_name_title="Шалкеровый ящик" item_minecraft_name_id="shulker_box" item_count="1"/>
     </div>
--- src/modules/pages/shopkeepers/Shopkeepers.js
+++ src/modules/pages/shopkeepers/Shopkeepers.js
@@ -1,20 +1,41 @@
 import React from "react";
+import {useAxios} from '../../../DataProvider';
+import Preload from "../../components/preloader/Preload";
 
 import "./Shopkeepers.scss";
 import ShulkerBox from "../../components/[0_grouped_0]-shopkeepers/shulker-box/Shulker-box";
 import OneSuggestions from "../../components/[0_grouped_0]-shopkeepers/one-suggestions/One-suggestions.js";
 
 const Shopkeepers = () => {
+  const resParams = useAxios(
+    "/api/get_shops",
+    'GET',
+    {}
+  );
+
+  if (resParams.loading) {
+    return <Preload/>;
+  }
+
+  const data = resParams.data;
 
   return (
     <div className="main-shopkeepers-block">
       <h4 className="title-shop-h4 font-custom-2">Товары игроков сервера</h4>
-      <div className="shop-block-center">
-        <div className="shop-all-suggestions">
-          <OneSuggestions/>
-        </div>
-        <div className="shop-one-suggestions"><ShulkerBox/></div>
-      </div>
+      {data.map((el, index) => {
+        return(
+          el.offers.map(offer => {
+            return (
+              <div className="shop-block-center">
+                <div className="shop-all-suggestions">
+                  <OneSuggestions amount={offer.amount} currency={offer.currency}/>
+                </div>
+                <div className="shop-one-suggestions"><ShulkerBox content={offer.content}/></div>
+              </div>
+            );
+          })
+        )
+      })}
     </div>
   );
 };
