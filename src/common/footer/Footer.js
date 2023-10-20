0981d0159ce644dfe44ad2cead5a0e709f5750f7
JuraRusan <rusanjura@gmail.com>
2023-10-20 13:44:39 +0300

    footer update

--- src/common/footer/Footer.js
+++ src/common/footer/Footer.js
@@ -1,12 +1,12 @@
+import classNames from "classnames";
 import React, {useEffect} from "react";
 import AOS from "aos";
-
-import "./Footer.scss";
-import "aos/dist/aos.css";
-
 import SvgDiscord from "../../bases/icons/SvgDiscord.js";
 import SvgInstagram from "../../bases/icons/SvgInstagram.js";
 import SvgVk from "../../bases/icons/SvgVk.js";
+
+import styles from "./Footer.module.scss";
+import "aos/dist/aos.css";
 
 const Footer = () => {
   useEffect(() => {
@@ -14,56 +14,27 @@ const Footer = () => {
   }, []);
 
   return (
-    <div className="footer">
-      <div className="rows-bt" data-aos="zoom-in">
-        <a className="bt-links" href="https://vk.com/gmgameru">
-          <SvgVk width="100%" height="100%" color="black"/>
-        </a>
-        <a className="bt-links" href="https://www.instagram.com/gmgameserver/">
-          <SvgInstagram width="100%" height="100%" color="black"/>
-        </a>
-        <a className="bt-links" href="https://discord.gg/cAM6tUYEuX">
-          <SvgDiscord width="100%" height="100%" color="black"/>
-        </a>
+    <div className={classNames(styles["footer"])}>
+      <div className={classNames(styles["rows-bt"])}>
+        <div className={classNames(styles["leftLine"])}></div>
+        <div className={classNames(styles["wrapperLink"])}>
+          <a className={classNames(styles["bt-links"])} href="https://vk.com/gmgameru">
+            <SvgVk width="100%" height="100%" color="white"/>
+          </a>
+          <a className={classNames(styles["bt-links"])} href="https://www.instagram.com/gmgameserver/">
+            <SvgInstagram width="100%" height="100%" color="white"/>
+          </a>
+          <a className={classNames(styles["bt-links"])} href="https://discord.gg/cAM6tUYEuX">
+            <SvgDiscord width="100%" height="100%" color="white"/>
+          </a>
+        </div>
+        <div className={classNames(styles["rightLine"])}></div>
       </div>
-      <div className="gmgame-title-f">
-        <h3 className="span loader">
-          <span>G</span>
-          <span>M</span>
-          <span>G</span>
-          <span>a</span>
-          <span>m</span>
-          <span>e</span>
-          <span>&nbsp;</span>
-          <span>M</span>
-          <span>i</span>
-          <span>n</span>
-          <span>e</span>
-          <span>c</span>
-          <span>r</span>
-          <span>a</span>
-          <span>f</span>
-          <span>t</span>
-          <span>&nbsp;</span>
-          <span>S</span>
-          <span>e</span>
-          <span>r</span>
-          <span>v</span>
-          <span>e</span>
-          <span>r</span>
-          <span>&nbsp;</span>
-          <span>©</span>
-          <span>&nbsp;</span>
-          <span>2</span>
-          <span>0</span>
-          <span>2</span>
-          <span>0</span>
-          <span>-</span>
-          <span>2</span>
-          <span>0</span>
-          <span>2</span>
-          <span>3</span>
+      <div className={classNames(styles["gmgame-title-f"])}>
+        <h3 className={classNames(styles["classText"])}>Мы на
+          <a href="https://mineserv.top/gmgame">Mineserv.top</a>
         </h3>
+        <h3 className={classNames(styles["classText"])}>© 2020 - 2023 GMGame Minecraft Server</h3>
       </div>
     </div>
   );
