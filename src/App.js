68c2ceb49c07adae2f4c847f8b2a1c5b49c57a67
JuraRusan <rusanjura@gmail.com>
2023-10-27 14:40:26 +0300

    update App.js and App.scss parameters

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ import "./App.scss";
 
 function App() {
 
+  window.onscroll = function() {
+    var header = document.getElementById('header');
+    if (window.pageYOffset > 0) {
+      header.classList.add('sticky-header');
+    } else {
+      header.classList.remove('sticky-header');
+    }
+  };
+
   return (
     <Router>
       <div id="dynamic" className="main-wrapper-bg">
-        <div className="class-z-index-10000-header"><Header/></div>
+        <div className="class-z-index-10000-header" id="header"><Header/></div>
         <div className="class-z-index-1000"><Routes/></div>
         <div className="class-z-index-10000-footer"><Footer/></div>
       </div>
