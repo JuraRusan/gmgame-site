9844a30c57a36b1ca972d0566e0d043308b9df81
JuraRusan <rusanjura@gmail.com>
2023-10-28 14:58:55 +0300

    image player cab

--- src/modules/components/[0_grouped_0]-Profile/player-cabinet/Player-cabinet.js
+++ src/modules/components/[0_grouped_0]-Profile/player-cabinet/Player-cabinet.js
@@ -1,13 +1,50 @@
 import classNames from "classnames";
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import styles from "./Player-cabinet.module.scss";
 
-const PlayerCabinet = (user) => {
+const PlayerCabinet = (props) => {
+
+  const sliceName = props.global
+  const [maxLength, setMaxLength] = useState(14)
+  const outName = sliceName.length > maxLength ? `${sliceName.slice(0, maxLength)}...` : sliceName;
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 960 && window.innerWidth <= 1024) {
+        setMaxLength(10);
+      } else if (window.innerWidth > 960) {
+        setMaxLength(14);
+      } else {
+        setMaxLength(33);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className={classNames(styles["playerWrapper"])}>
-      <img className={classNames(styles["playerImage"])} src={`https://minotar.net/helm/${user.username}/100`} alt=""/>
-      <h5 className={classNames(styles["namePlayerH5"])}>{user.username}</h5>
+      <div className={classNames(styles["rowUser"])}>
+        <img
+          className={classNames(styles["image"])}
+          src={`https://minotar.net/helm/${props.username}/100`}
+          alt=""
+        />
+        <h5 className={classNames(styles["namePlayerH5"])}>{props.username}</h5>
+      </div>
+      <div className={classNames(styles["rowUser"])}>
+        <img
+          className={classNames(styles["image"])}
+          src={`https://cdn.discordapp.com/avatars/${props.id}/${props.avatar}.png`}
+          alt="none"
+        />
+        <h5 className={classNames(styles["namePlayerH5"])}>@{outName}</h5>
+      </div>
     </div>
   );
 }
