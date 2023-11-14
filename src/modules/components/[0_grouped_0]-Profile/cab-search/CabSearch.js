22ce906c5109e06c0b38570797e74bc260c3b5d6
JuraRusan <rusanjura@gmail.com>
2023-11-14 22:50:25 +0200

    add btn

--- src/modules/components/[0_grouped_0]-Profile/cab-search/CabSearch.js
+++ src/modules/components/[0_grouped_0]-Profile/cab-search/CabSearch.js
@@ -1,14 +1,18 @@
 import classNames from "classnames";
 import React from "react";
+import {Link} from "react-router-dom";
+import MapSvgAddBlock from "../../[0_grouped_0]-Maps-all-comp/mini-marker-components/map-svg-add-block/MapSvgAddBlock";
 
 import styles from "./CabSearch.module.scss";
-import "aos/dist/aos.css";
 
-const CabSearch = (props) => {
+const CabSearch = ({onChange, name, count, to}) => {
   return (
     <div className={classNames(styles["boxSearchWrapper"])}>
-      <input className={classNames(styles["searchInput"])} onChange={props.onChange} type="search" placeholder="Поиск"/>
-      <h4 className={classNames(styles["numberListCount"])}>Количество {props.name} - {props.count}</h4>
+      <input className={classNames(styles["searchInput"])} onChange={onChange} type="search" placeholder="Поиск"/>
+      <h4 className={classNames(styles["numberListCount"])}>Количество {name} - {count}</h4>
+      <Link to={to} className={classNames(styles["margin_btn"])}>
+        <MapSvgAddBlock/>
+      </Link>
     </div>
   );
 }
