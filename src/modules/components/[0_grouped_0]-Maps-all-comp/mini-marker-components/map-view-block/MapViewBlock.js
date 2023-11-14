fc7b8068d1b33483affe756a11fdc6a1447ad748
JuraRusan <rusanjura@gmail.com>
2023-11-14 22:51:45 +0200

    fix view one map element

--- src/modules/components/[0_grouped_0]-Maps-all-comp/mini-marker-components/map-view-block/MapViewBlock.js
+++ src/modules/components/[0_grouped_0]-Maps-all-comp/mini-marker-components/map-view-block/MapViewBlock.js
@@ -2,25 +2,21 @@ import classNames from "classnames";
 import React from "react";
 import {Link} from 'react-router-dom';
 import BinSvgComponent from "../../../../../bases/icons/binSVG/binSvg";
-import SvgSettings from "../../../../../bases/icons/SvgSettings";
 
 import styles from "./MapViewBlock.module.scss";
 
-const MapViewBlock = (props) => {
+const MapViewBlock = ({link_to, key, index, name, onClick}) => {
   return (
-    <div key={props.key} className={classNames(styles["oneMapsElement"])}>
-      <div className={classNames(styles["columnsOne"])}>
-        <p className={classNames(styles["elementIndex"])}>{props.index + 1}</p>
-        <h3 className={classNames(styles["elementH3Name"])}>{props.name}</h3>
-      </div>
-      <div className={classNames(styles["columnsTwo"])}>
-        <Link to={props.link_to} className={classNames(styles["actions"], styles["hover_settings"])}>
-          <SvgSettings width="100%" height="100%" color="#f4f4f4"/>
-        </Link>
-        <button className={classNames(styles["actions"], styles["hover_delete"])} onClick={props.onClick}>
-          <BinSvgComponent width="100%" height="100%" color="#f4f4f4"/>
-        </button>
-      </div>
+    <div key={key} className={classNames(styles["one_maps_element"])}>
+      <Link to={link_to} className={classNames(styles["link_to"])}>
+        <div className={classNames(styles["wrapper"])}>
+          <p className={classNames(styles["elementIndex"])}>{index + 1}</p>
+          <h3 className={classNames(styles["elementH3Name"])}>{name}</h3>
+        </div>
+      </Link>
+      <button className={classNames(styles["actions"], styles["hover_delete"])} onClick={onClick}>
+        <BinSvgComponent width="100%" height="100%" color="#f4f4f4"/>
+      </button>
     </div>
   );
 };
