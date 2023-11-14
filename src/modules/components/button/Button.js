07a2c7fc361c43398463412311f800e8bd15d717
JuraRusan <rusanjura@gmail.com>
2023-11-14 22:48:22 +0200

    btn comp

--- src/modules/components/button/Button.js
+++ src/modules/components/button/Button.js
@@ -1,12 +1,23 @@
 import classNames from "classnames";
 import React from "react";
-import {Link} from 'react-router-dom';
 
 import styles from "./Button.module.scss";
 
-const Button = () => {
+const Button = ({onClick, label, disabled, type, id, view}) => {
   return (
-    <></>
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      type={type}
+      id={id}
+      className={classNames(styles["btn"],
+        {
+          [styles["submit"]]: view === "submit",
+          [styles["delete"]]: view === "delete",
+          [styles["view"]]: view === "view",
+        }
+      )}
+    >{label}</button>
   );
 };
 
