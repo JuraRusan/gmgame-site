e697d7b43c90ca7de80425df0dce660704b30ea0
JuraRusan <rusanjura@gmail.com>
2023-12-05 17:18:02 +0200

    fix link

--- src/modules/components/[0_grouped_0]-Maps-all-comp/mini-marker-components/function/CheckForm.js
+++ src/modules/components/[0_grouped_0]-Maps-all-comp/mini-marker-components/function/CheckForm.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Notifications from "../../../notifications/Notifications";
+import NotificationsSvgComponent from "../../../../../bases/icons/notificationsSvg/NotificationsSvg";
 
 export function checkForm(number, setErrorMessage) {
   const cord = number.trim();
   if (!/^[0-9+-]+$/.test(cord)) {
-    setErrorMessage(<Notifications inf="Только числа" type="error"/>)
+    setErrorMessage(<NotificationsSvgComponent inf="Только числа" type="error"/>)
   } else {
     setErrorMessage(null);
   }
--- src/modules/components/[0_grouped_0]-Maps-all-comp/mini-marker-components/function/CheckName.js
+++ src/modules/components/[0_grouped_0]-Maps-all-comp/mini-marker-components/function/CheckName.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Notifications from "../../../notifications/Notifications";
+import NotificationsSvgComponent from "../../../../../bases/icons/notificationsSvg/NotificationsSvg";
 
 export function checkName(name, setErrorMessage) {
   const label = name.trim();
   if (label.length < 5 || label.length > 300) {
-    setErrorMessage(<Notifications inf="Имя должно содержать от 5 до 300 символов." type="error"/>);
+    setErrorMessage(<NotificationsSvgComponent inf="Имя должно содержать от 5 до 300 символов." type="error"/>);
   } else {
     setErrorMessage(null);
   }
