4c8177fa55f93e7606fbf1efefcda2cb2c8cefc5
JuraRusan <rusanjura@gmail.com>
2023-12-17 22:06:34 +0200

    test new logo

---
+++ src/bases/icons/LogoMainText.js
@@ -0,0 +1,402 @@
+import * as React from "react"
+
+const LogoMainTextSvgComponent = ({width, height}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 286.921 78.346"
+    width={width}
+    height={height}
+  >
+    <defs>
+      <linearGradient
+        xlinkHref="#a"
+        id="j"
+        x1={1909.223}
+        x2={1909.223}
+        y1={-221.23}
+        y2={-177.449}
+        gradientTransform="translate(-1789.864 285.488)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <linearGradient id="a">
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#c3b3cc",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#e4dcf2",
+            stopOpacity: 1,
+          }}
+        />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#b"
+        id="k"
+        x1={1854.138}
+        x2={1854.138}
+        y1={-221.23}
+        y2={-175.442}
+        gradientTransform="translate(-1789.864 285.488)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <linearGradient id="b">
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#84079c",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#e944ee",
+            stopOpacity: 1,
+          }}
+        />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#c"
+        id="l"
+        x1={1828.554}
+        x2={1828.554}
+        y1={-221.23}
+        y2={-177.448}
+        gradientTransform="translate(-1789.864 285.488)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <linearGradient id="c">
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#fd9416",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#f3ba41",
+            stopOpacity: 1,
+          }}
+        />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#d"
+        id="m"
+        x1={1815.618}
+        x2={1815.618}
+        y1={-177.448}
+        y2={-166.011}
+        gradientTransform="translate(-1789.864 285.488)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <linearGradient id="d">
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#5f451d",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#a5782d",
+            stopOpacity: 1,
+          }}
+        />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#e"
+        id="n"
+        x1={1882.327}
+        x2={1882.327}
+        y1={-177.449}
+        y2={-166.011}
+        gradientTransform="translate(-1789.864 285.488)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <linearGradient id="e">
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#551c5b",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#942c9c",
+            stopOpacity: 1,
+          }}
+        />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#f"
+        id="o"
+        x1={1847.62}
+        x2={1847.62}
+        y1={-177.448}
+        y2={-166.011}
+        gradientTransform="translate(-1789.864 285.488)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <linearGradient id="f">
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#561c5c",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#c73fcc",
+            stopOpacity: 1,
+          }}
+        />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#g"
+        id="p"
+        x1={1906.295}
+        x2={1906.295}
+        y1={-177.448}
+        y2={-166.011}
+        gradientTransform="translate(-1789.864 285.488)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <linearGradient id="g">
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#625b6f",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#887e95",
+            stopOpacity: 1,
+          }}
+        />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#h"
+        id="q"
+        x1={1826.932}
+        x2={1826.932}
+        y1={-166.011}
+        y2={-177.448}
+        gradientTransform="translate(-1789.864 285.488)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <linearGradient id="h">
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#cd9c3f",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#5e441d",
+            stopOpacity: 1,
+          }}
+        />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#i"
+        id="r"
+        x1={1909.223}
+        x2={1909.223}
+        y1={-177.449}
+        y2={-166.011}
+        gradientTransform="translate(-1789.864 285.488)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <linearGradient id="i">
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#625a6e",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#cbc0db",
+            stopOpacity: 1,
+          }}
+        />
+      </linearGradient>
+    </defs>
+    <path
+      d="M20.038 59.622 18.97 62.77 3.052 109.696l18.78 14.42H75.54l1.726-9.612h3.59l5.713 9.612H192.99l4.72-9.612h4.415l1.59 9.612h53.869l17.412-14.521-5.11-15.803h-4.448l3.467-3.039-4.798-14.84h-3.233l2.491-2.291-4.525-14H204.673l-5.228 8.683.493-.724h-5.03l-1.08-7.959H85.065l-1.21 7.959-3.374.102-4.311-8.06H54.738Z"
+      style={{
+        color: "#000",
+        InkscapeFontSpecification: "Minercraftory",
+        fill: "#22171d",
+        fillOpacity: 1,
+        strokeWidth: 9.51309,
+        strokeDasharray: "none",
+        paintOrder: "markers fill stroke",
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="M23.362 64.258 8.51 108.04l14.897 11.438h30.707l.856-3.818 3.53 3.818h13.161l2.582-14.376 3.424 4.766h5.828l5.713 9.61h13.16l.88-8.647 3.507 8.647h30.707l.348-27.49-.595-8.331h.7l.101-7.96-.792-11.439H106.57l-.88 8.645-3.5-8.645H89.053l-1.208 7.96h-4.48l-1.367 8.331h-3.344l.872-4.852-7.99-11.439H58.395l-.947 3.961-3.432-3.96zm118.242 0-.075 43.782.32 11.438h13.16l1.3-11.438-.262-7.325h2.543l.806 18.763h13.16l3.403-11.252.985 11.252h13.16l4.72-9.61h6.957l3.375-5.471 2.495 15.08h13.16l3.494-4.252.893 4.253h30.707l13.716-11.438-3.107-9.61H253.11l10.444-9.153-2.822-8.728h-11.758l9.065-8.33-2.573-7.96H224.81l-3.394 4.409-.985-4.41h-13.138l-6.886 11.439.424 2.561-1.314 2.291h-2.946l-1.235-8.33h-4.48l-1.08-7.96h-13.138l-3.427 11.11-.952-11.11z"
+      style={{
+        fontSize: "50.8px",
+        fontFamily: "Minercraftory",
+        InkscapeFontSpecification: "Minercraftory",
+        fill: "#22171d",
+        fillOpacity: 1,
+        strokeWidth: 0.975204,
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="m106.57 64.258-4.454 43.782h34.486l.39-27.49h-13.748l-.925 17.88h-4.807l1.684-26.211h17.915l.113-7.96zm35.034 0-.075 43.782h14.78l-.672-18.763h4.692l.906 18.763h14.78l-3.755-43.782Zm35.034 0 4.303 43.782h14.78l-2.547-18.763h4.692l1.357 9.152h9.613l-1.586-9.152h4.692l3.486 18.763h14.78l-9.776-43.782h-13.138l1.479 7.96h-4.48l1.445 8.331h-9.166l-1.236-8.33h-4.479l-1.08-7.96zm48.173 0 10.323 43.782h34.486l-3.106-9.61h-19.226l-2.502-9.153h18.769l-2.822-8.728h-18.332l-2.276-8.33h17.915l-2.574-7.96zm-69.784 7.96h4.479l.402 8.331h-4.583z"
+      style={{
+        fontSize: "50.8px",
+        fontFamily: "Minercraftory",
+        InkscapeFontSpecification: "Minercraftory",
+        fill: "url(#j)",
+        fillOpacity: 1,
+        strokeWidth: 1.03347,
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="M58.396 64.258h13.138l-1.606 7.96h4.48L72.83 80.55h9.166l1.368-8.33h4.479l1.208-7.96h13.138l-5.001 43.78h-14.78l2.847-18.762h-4.692l-1.503 9.152h-9.613l1.732-9.152h-4.692l-3.785 18.763h-14.78Z"
+      style={{
+        fill: "url(#k)",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 1.8,
+        strokeLinecap: "round",
+        strokeDasharray: "none",
+        strokeOpacity: 0.413559,
+        paintOrder: "markers fill stroke",
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="m49.916 80.55-6.92 27.49H8.51l14.852-43.782h30.655l-2.004 7.96H34.098L26.189 98.43h4.807l5.171-17.88z"
+      style={{
+        fill: "url(#l)",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 1.8,
+        strokeLinecap: "round",
+        strokeDasharray: "none",
+        strokeOpacity: 0.413559,
+        paintOrder: "markers fill stroke",
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="m8.51 108.04 14.897 11.438h30.707L42.996 108.04Z"
+      style={{
+        fill: "url(#m)",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 1,
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="m47.923 108.04 10.578 11.438h13.16l-8.959-11.438zm34.486 0 6.799 11.438h13.16l-5.18-11.438z"
+      style={{
+        fill: "url(#n)",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 1,
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="m47.923 108.04 9.666 10.451.912.987h13.16l-8.959-11.438h-1.448l8.186 10.451H59.037l-9.665-10.451zm34.486 0 6.212 10.451.587.987h13.16l-.446-.987-4.733-10.451H95.74l4.733 10.451H90.07l-6.212-10.451z"
+      style={{
+        fill: "url(#o)",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 9.27499,
+        strokeLinecap: "round",
+        strokeDasharray: "none",
+        strokeOpacity: 0.413559,
+        paintOrder: "markers fill stroke",
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="m102.116 108.04 4.64 11.438h30.706l-.86-11.438zm39.413 0 .32 11.438h13.16l1.3-11.438zm19.706 0-1.84 11.438h13.161l3.459-11.438zm19.706 0-3.998 11.438h13.16l5.618-11.438zm34.487 0-7.778 11.438h13.16l9.398-11.438zm19.706 0-9.936 11.438h30.707l13.716-11.438z"
+      style={{
+        fill: "url(#p)",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 1,
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="M39.928 64.258C31.483 69.161 29.584 70.26 19.3 76.235L8.51 108.04h26.815l9.181-6 5.41-21.49H36.167l-5.171 17.88H26.19l7.908-26.211h17.915l2.004-7.96zm18.468 0-8.139 34.025 27.14-17.734H72.83l1.576-8.33h-4.479l1.606-7.96zm30.656 0-1.208 7.96h-4.479l-.815 4.964 17.263-11.28s1.48-.52 2.28-.808l.097-.836zm74.123 0c-8.84 5.132-10.804 6.269-21.593 12.537l-.053 31.245h14.78l-.671-18.763h4.692l.824 17.076 13.936-9.106-2.83-32.989zm13.463 0 2.954 30.048 21.052-13.757h-4.073l-1.235-8.33h-4.478l-1.082-7.96zm30.656 0 1.479 7.96h-4.48l.926 5.342 15.868-10.368-.655-2.934zm17.517 0 .223.945c.839-.303 1.293-.474 2.667-.945zm-69.784 7.96h4.478l.403 8.331h-4.583zm-19.905 8.331c-4.432 2.576-7.973 4.64-12.246 7.125l-.557 10.755h-4.806l.51-7.932c-5.091 2.961-10.631 6.18-15.011 8.732l-.896 8.81h34.486l.39-27.49zm125.115 0c-6.459 3.752-8.452 4.911-15.018 8.728h18.335l-2.82-8.728zm-15.391 8.945c-4.459 2.591-8.15 4.741-12.386 7.205l2.674 11.34h34.487l-3.108-9.61h-19.225zm-16.62 9.668c-4.326 2.517-9.287 5.399-13.039 7.585l.24 1.293h14.78zm-130.374 3.072a6136.478 6136.478 0 0 0-9.937 5.806h9.274z"
+      style={{
+        opacity: 0.170732,
+        fill: "#fff",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 9.27499,
+        strokeLinecap: "round",
+        strokeDasharray: "none",
+        strokeOpacity: 0.413559,
+        paintOrder: "markers fill stroke",
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="m8.51 108.04 14.897 11.438h30.707l-.96-.987-10.158-10.451h-1.448l10.158 10.451H23.57L9.96 108.04Z"
+      style={{
+        fill: "url(#q)",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 9.27499,
+        strokeLinecap: "round",
+        strokeDasharray: "none",
+        strokeOpacity: 0.413559,
+        paintOrder: "markers fill stroke",
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+    <path
+      d="m102.116 108.04 4.24 10.451.4.987h30.706l-.074-.987-.786-10.451h-1.449l.786 10.451h-28.135l-4.24-10.451zm39.413 0 .293 10.451.027.987h13.16l1.308-11.438h-1.45l-1.194 10.451H143.27l-.293-10.451zm19.707 0-1.681 10.451-.16.987H172.558l3.458-11.438h-1.448l-3.162 10.451h-10.402l1.681-10.451zm19.705 0-3.653 10.451-.345.987h13.16l5.618-11.438h-1.448l-5.133 10.451h-10.403l3.653-10.451zm34.487 0-7.106 10.451h-.001l-.67.987h13.16l.81-.987 8.587-10.451h-1.449l-8.587 10.451H209.77l7.106-10.451zm19.706 0-9.08 10.451-.856.987h30.707l13.716-11.438h-1.45L255.64 118.49h-28.136l9.08-10.451z"
+      style={{
+        fill: "url(#r)",
+        fillOpacity: 1,
+        stroke: "none",
+        strokeWidth: 105.675,
+        strokeLinecap: "round",
+        strokeDasharray: "none",
+        strokeOpacity: 0.413559,
+        paintOrder: "markers fill stroke",
+      }}
+      transform="translate(4.436 -52.696)"
+    />
+  </svg>
+)
+
+export default LogoMainTextSvgComponent
