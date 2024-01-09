1247e8b722a6910636c195904c1a20829c55faa6
JuraRusan <rusanjura@gmail.com>
2024-01-09 21:23:01 +0200

    event stats

---
+++ src/modules/pages/event/data/house5.js
@@ -0,0 +1,370 @@
+export const house5Status = [
+  {
+    "name": "prestig9110",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "_BaXy_",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 8192,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "_KreST1k",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "bossneboss",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "kermitik",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "IDrakon4ik",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "etteki",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "SoftPanda3",
+    "christmas_house_5_snow_block_lead": 122880,
+    "christmas_house_5_ice_block_lead": 122944,
+    "christmas_house_5_wood_block_lead": 71680,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 4096
+  },
+  {
+    "name": "IronCAT_",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "_scweppes_",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "3_3loi_3",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "MR_abricos2017",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Mad_Chiffa",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 51200,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Mumongi",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "SenSid",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "SolyankaXD",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "gerhugwsif",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Lucky_Sword",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "yurchiq",
+    "christmas_house_5_snow_block_lead": 171200,
+    "christmas_house_5_ice_block_lead": 54848,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 16512,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "BlackPirat",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Quilstin",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Stavrik",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Awan_Weller",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "BeaST2517",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "_PrometheuZ_",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "artim112",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "KielkaW13",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "TheMuvin",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Steblyasha",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Qberty22",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "HuDreY",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "G1eb04k0",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "_Kerubifi_",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 67968,
+    "christmas_house_5_wood_block_lead": 122880,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 8192
+  },
+  {
+    "name": "Sera228Lite",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "EUGENlJUS",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "lus7",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 64,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "matttvey",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Dalandis",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Star_Braid",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Hackerterminator",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 256,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "llix0o0",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "Sensuha",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "RegalManiac",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "MrkerichYT",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "ThreeASix",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  },
+  {
+    "name": "TarosLove",
+    "christmas_house_5_snow_block_lead": 0,
+    "christmas_house_5_ice_block_lead": 0,
+    "christmas_house_5_wood_block_lead": 0,
+    "christmas_house_5_present_item_lead": 0,
+    "christmas_house_5_snowman_lead": 0
+  }
+];
---
+++ src/modules/pages/event/data/tree2.js
@@ -0,0 +1,370 @@
+export const tree2Status = [
+  {
+    "name": "prestig9110",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "_BaXy_",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 138240,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 21568,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "_KreST1k",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 320,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "bossneboss",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "kermitik",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "IDrakon4ik",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 128,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "etteki",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "SoftPanda3",
+    "christmas_tree_2_snow_block_lead": 412672,
+    "christmas_tree_2_ice_block_lead": 289792,
+    "christmas_tree_2_wood_block_lead": 242688,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "IronCAT_",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 7552,
+    "christmas_tree_2_snowman_lead": 4096
+  },
+  {
+    "name": "_scweppes_",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 192,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "3_3loi_3",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "MR_abricos2017",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Mad_Chiffa",
+    "christmas_tree_2_snow_block_lead": 23552,
+    "christmas_tree_2_ice_block_lead": 2048,
+    "christmas_tree_2_wood_block_lead": 57344,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Mumongi",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "SenSid",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "SolyankaXD",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "gerhugwsif",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Lucky_Sword",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "yurchiq",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 7040,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "BlackPirat",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Quilstin",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Stavrik",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Awan_Weller",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "BeaST2517",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "_PrometheuZ_",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "artim112",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "KielkaW13",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "TheMuvin",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Steblyasha",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Qberty22",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "HuDreY",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "G1eb04k0",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "_Kerubifi_",
+    "christmas_tree_2_snow_block_lead": 110592,
+    "christmas_tree_2_ice_block_lead": 122880,
+    "christmas_tree_2_wood_block_lead": 237568,
+    "christmas_tree_2_present_item_lead": 2176,
+    "christmas_tree_2_snowman_lead": 22528
+  },
+  {
+    "name": "Sera228Lite",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "EUGENlJUS",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "lus7",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "matttvey",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Dalandis",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Star_Braid",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Hackerterminator",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "llix0o0",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "Sensuha",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "RegalManiac",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "MrkerichYT",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "ThreeASix",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  },
+  {
+    "name": "TarosLove",
+    "christmas_tree_2_snow_block_lead": 0,
+    "christmas_tree_2_ice_block_lead": 0,
+    "christmas_tree_2_wood_block_lead": 0,
+    "christmas_tree_2_present_item_lead": 0,
+    "christmas_tree_2_snowman_lead": 0
+  }
+];
