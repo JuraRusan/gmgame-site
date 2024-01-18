f328f6597977325dc1e0c5658a556a706a2eea9f
dalandis <varganov.a.p@gmail.com>
2024-01-18 21:26:34 +0400

    add citizenships

--- src/modules/components/[0_grouped_0]-Manager/Player-summary/Player-summary.js
+++ src/modules/components/[0_grouped_0]-Manager/Player-summary/Player-summary.js
@@ -203,7 +203,10 @@ const PlayerSummary = () => {
     let valueDate = '';
     if (event.target.id === 'expiration_date') {
       valueDate = new Date(event.target.value).toISOString();
-      console.log(valueDate)
+    }
+
+    if (event.target.id === 'citizenship') {
+      valueDate = event.target.value === 'true' ? true : false;
     }
 
     input[id] = {...input[id], ...{[event.target.id]: valueDate || event.target.value}};
@@ -748,6 +751,17 @@ const PlayerSummary = () => {
                   onChange={(e) => userDetailsChange(e, userDetails.user_id)}
                   className={classNames(styles["inputUserRedactor"])}
                   defaultValue={userDetails?.immun}
+                />
+                <span className={classNames(styles["viewRedactor"])}>&#10043;</span>
+              </div>
+              {/*----- citizenship -----*/}
+              <div className={classNames(styles["oneBlock"])}>
+                <p className={classNames(styles["textDescriptionParagraph"])}>Гражданство:</p>
+                <input
+                  id="citizenship"
+                  onChange={(e) => userDetailsChange(e, userDetails.user_id)}
+                  className={classNames(styles["inputUserRedactor"])}
+                  defaultValue={userDetails?.citizenship}
                 />
                 <span className={classNames(styles["viewRedactor"])}>&#10043;</span>
               </div>
