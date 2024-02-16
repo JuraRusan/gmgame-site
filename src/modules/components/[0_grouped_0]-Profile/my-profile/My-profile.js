47a431f3e4771dadd8984ed60009c76a6c853582
JuraRusan <rusanjura@gmail.com>
2024-02-16 18:02:03 +0200

    added some text

--- src/modules/components/[0_grouped_0]-Profile/my-profile/My-profile.js
+++ src/modules/components/[0_grouped_0]-Profile/my-profile/My-profile.js
@@ -10,7 +10,6 @@ import { sendRequest } from "../../../../DataProvider";
 import Button from "../../button/Button";
 
 import styles from "./My-profile.module.scss";
-import "aos/dist/aos.css";
 
 const MyProfile = () => {
   const alert = useAlert();
@@ -51,84 +50,48 @@ const MyProfile = () => {
       <div className={classNames(styles["content"])}>
         {data.user.status === 1 ? (
           <div className={classNames(styles["wrapperText"])}>
-            <h4 className={classNames(styles["text"])}>
-              Ваша заявка на рассмотрении. Пожалуйста ожидайте.
-            </h4>
+            <h4 className={classNames(styles["text"])}>Ваша заявка на рассмотрении. Пожалуйста ожидайте.</h4>
           </div>
-        ) : undefined}
+        ) : null}
 
         {data.user.status === 2 ? (
           <div className={classNames(styles["wrapperText"])}>
-            <h4 className={classNames(styles["text"], styles["good"])}>
-              Ваша заявка одобрена. Добро пожаловать на сервер!
-            </h4>
-            <h4 className={classNames(styles["text"])}>
-              Пожалуйста, ознакомьтесь с правилами и разделом часто задаваемые
-              вопросы для получения полезной информации.
-            </h4>
+            <h4 className={classNames(styles["text"], styles["good"])}>Ваша заявка одобрена. Добро пожаловать на сервер!</h4>
+            <h4 className={classNames(styles["text"])}>Пожалуйста, ознакомьтесь с правилами, вики и разделом часто задаваемые вопросы для получения полезной информации.</h4>
             <div className={classNames(styles["version"])}>
-              <span className={classNames(styles["versionNumber"])}>
-                Java Edition {data.version}
-              </span>
+              <span className={classNames(styles["versionNumber"])}>Java Edition {data.version}</span>
             </div>
             <div className={classNames(styles["ipList"])}>
-              <label className={classNames(styles["label"])}>
-                Серверные адреса для подключения:
-              </label>
+              <label className={classNames(styles["label"])}>Серверные адреса для подключения:</label>
               {ipArray.map((el, index) => (
-                <div
-                  className={classNames(styles["wrapper"])}
-                  key={index}
-                  onClick={() => handleCopyClick(index)}
-                >
-                  <label
-                    className={classNames(styles["ip"], {
-                      [styles["ipTo"]]: copiedIndex === index,
-                    })}
-                  >
-                    {el}
-                  </label>
+                <div className={classNames(styles["wrapper"])} key={index} onClick={() => handleCopyClick(index)}>
+                  <label className={classNames(styles["ip"], {[styles["ipTo"]]: copiedIndex === index,})}>{el}</label>
                   <div className={classNames(styles["copy"])}>
-                    {copiedIndex === index ? (
-                      <span
-                        className={classNames(styles["ico"], styles["icoTo"])}
-                      >
-                        <CheckSvgComponent
-                          width="100%"
-                          height="100%"
-                          color="#f4f4f4"
-                        />
-                      </span>
-                    ) : (
-                      <span className={classNames(styles["ico"])}>
-                        <CopySvgComponent
-                          width="100%"
-                          height="100%"
-                          color="#f4f4f4"
-                        />
-                      </span>
-                    )}
+                    {copiedIndex === index
+                      ? (<span className={classNames(styles["ico"], styles["icoTo"])}><CheckSvgComponent width="100%" height="100%" color="#f4f4f4"/></span>)
+                      : (<span className={classNames(styles["ico"])}><CopySvgComponent width="100%" height="100%" color="#f4f4f4"/></span>)
+                    }
                   </div>
                 </div>
               ))}
             </div>
           </div>
-        ) : undefined}
+        ) : null}
 
         {data.user.status === 3 ? (
           <div className={classNames(styles["wrapperText"])}>
-            <h4 className={classNames(styles["text"])}>
-              Ваша заявка отклонена. Спасибо за понимание.
-            </h4>
-            {!data.user.reapplication && data.user.status === 3 ? (
-              <div className={classNames(styles["user_update_statement"])}>
-                <Button view="submit" label="Переподать заявку" onClick={() => resubmit()}/>
-              </div>
-            ) : (
-              ""
-            )}
+            <h4 className={classNames(styles["text"])}>Ваша заявка отклонена. Спасибо за понимание.</h4>
+            {data.user.reapplication && data.user.status === 3 ?
+              <>
+                <div className={classNames(styles["user_update_statement"])}>
+                  <Button view="submit" label="Переподать заявку" onClick={() => resubmit()}/>
+                </div>
+                <h4 className={classNames(styles["text"])}>Чтобы увеличить вероятность вашего принятия, пожалуйста, уделите больше внимания деталям в заявке.</h4>
+              </>
+              : null
+            }
           </div>
-        ) : undefined}
+        ) : null}
 
         {data.user.status === 4 ? (
           <img
@@ -138,7 +101,7 @@ const MyProfile = () => {
             width="100%"
             height="auto"
           />
-        ) : undefined}
+        ) : null}
 
         {data.user.status === 5 ? (
           <div className={classNames(styles["wrapperText"])}>
