a8a9c1f52ad32b2c11c4535bfa0dd1e6da2e0afb
JuraRusan <rusanjura@gmail.com>
2025-03-31 11:36:24 +0300

    обновление кода, функциональность не изменилась.

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,7 +1,6 @@
-import classNames from "classnames";
-import React, { useEffect, useState } from "react";
-import { sendRequest, useAxios } from "../../DataProvider";
-import AOS from "aos";
+import cN from "classnames";
+import React from "react";
+import useHeader from "./useHeader";
 import LogoMainTextSvgComponent from "../../bases/icons/LogoMainText";
 import GoOutSvgComponent from "../../bases/icons/goOutSvg/GoOutSvg";
 import ManagerSvgComponent from "../../bases/icons/managerSvg/ManagerSvg";
@@ -12,49 +11,19 @@ import styles from "./Header.module.scss";
 import "aos/dist/aos.css";
 
 const Header = () => {
-  const resParams = useAxios("/api/profile", "GET", {});
-
-  const [cabDropMenu, setCabDropMenu] = useState(false);
-  const [mainDropMenu, setMainDropMenu] = useState(false);
-
-  const closeMenuMain = () => {
-    setMainDropMenu(false);
-  };
-
-  const closeMenuCab = () => {
-    setCabDropMenu(false);
-  };
-
-  const logout = () => {
-    sendRequest("/api/logout", "POST", {}).then((response) => {
-      localStorage.clear();
-      window.location.href = "/";
-    });
-  };
+  const { cabDropMenu, mainDropMenu, resParams, logout, setCabDropMenu, setMainDropMenu } = useHeader();
 
   const DiscordIcon = () => {
-    if (resParams.data.discordUser.avatar === null) {
-      return (
-        <img
-          className={classNames(styles["icon_player"])}
-          src={`../site_assets/pages/webp/avatar_undefined.webp`}
-          alt="none"
-        />
-      );
-    } else {
-      return (
-        <img
-          className={classNames(styles["icon_player"])}
-          src={`https://cdn.discordapp.com/avatars/${resParams.data.discordUser.id}/${resParams.data.discordUser.avatar}.png`}
-          alt="none"
-        />
-      );
-    }
+    const imgLink = !resParams.data.discordUser.avatar
+      ? "../site_assets/pages/webp/avatar_undefined.webp"
+      : `https://cdn.discordapp.com/avatars/${resParams.data.discordUser.id}/${resParams.data.discordUser.avatar}.png`;
+
+    return <img className={styles["icon_player"]} src={imgLink} alt="none" />;
   };
 
   const Swipe = ({ to, name }) => {
     return (
-      <a className={classNames(styles["swipe_page"])} href={to}>
+      <a className={styles["swipe_page"]} href={to}>
         {name}
       </a>
     );
@@ -64,83 +33,49 @@ const Header = () => {
     if (!button) {
       return (
         <a
-          className={classNames(styles["list"], phone_show && styles["phone_list"])}
+          className={cN(styles["list"], {
+            [styles["phone_list"]]: phone_show,
+          })}
           href={href}
           {...(href.startsWith("http") ? { target: "_blank", rel: "noreferrer" } : {})}
           {...props}
         >
-          <span className={classNames(styles["icon_list"])}>{icon}</span>
+          <span className={styles["icon_list"]}>{icon}</span>
           {name}
         </a>
       );
     } else {
       return (
-        <button className={classNames(styles["list"])} {...props}>
-          <span className={classNames(styles["icon_list"])}>{icon}</span>
+        <button className={styles["list"]} {...props}>
+          <span className={styles["icon_list"]}>{icon}</span>
           {name}
         </button>
       );
     }
   };
 
-  useEffect(() => {
-    if (cabDropMenu) {
-      document.addEventListener("click", closeMenuCab);
-    } else {
-      document.removeEventListener("click", closeMenuCab);
-    }
-
-    return () => {
-      document.removeEventListener("click", closeMenuCab);
-    };
-  }, [cabDropMenu]);
-
-  useEffect(() => {
-    if (mainDropMenu) {
-      document.addEventListener("click", closeMenuMain);
-    } else {
-      document.removeEventListener("click", closeMenuMain);
-    }
-
-    return () => {
-      document.removeEventListener("click", closeMenuMain);
-    };
-  }, [mainDropMenu]);
-
-  useEffect(() => {
-    AOS.init({ duration: 1000 });
-  }, []);
-
   return (
-    <div className={classNames(styles["header"])}>
-      <div className={classNames(styles["main_container"])}>
-        <div className={classNames(styles["left_block"], styles["container_numbered"])}>
-          <a className={classNames(styles["logo"])} href="/">
+    <div className={styles["header"]}>
+      <div className={styles["main_container"]}>
+        <div className={cN(styles["left_block"], styles["container_numbered"])}>
+          <a className={styles["logo"]} href="/">
             <LogoMainTextSvgComponent height="100%" width="100%" />
           </a>
         </div>
-        <div className={classNames(styles["center_block"], styles["container_numbered"])}>
+        <div className={cN(styles["center_block"], styles["container_numbered"])}>
           <Swipe to="/regulations" name="Правила" />
-          <div className={classNames(styles["dropdown_menu_button"])} onClick={() => setMainDropMenu(!mainDropMenu)}>
-            {mainDropMenu === false ? (
-              <span className={classNames(styles["ico"])}>&#9776;</span>
-            ) : (
-              <span className={classNames(styles["ico"])}>&#10006;</span>
-            )}
-            {mainDropMenu === false ? null : (
-              <div
-                className={classNames(styles["dropdown_menu_main"])}
-                data-aos="zoom-out-down"
-                data-aos-duration="250"
-              >
-                <div className={classNames(styles["box_page_list"])}>
-                  <ListMenu phone_show={true} name="Правила" href="/regulations" />
-                  <ListMenu phone_show={true} name="Вопросы" href="/faq" />
+          <div className={styles["dropdown_menu_button"]} onClick={() => setMainDropMenu((prev) => !prev)}>
+            <span className={styles["ico"]}>{!mainDropMenu ? "\u2630" : "\u2716"}</span>
+            {!mainDropMenu ? null : (
+              <div className={styles["dropdown_menu_main"]} data-aos="zoom-out-down" data-aos-duration="250">
+                <div className={styles["box_page_list"]}>
+                  <ListMenu phone_show name="Правила" href="/regulations" />
+                  <ListMenu phone_show name="Вопросы" href="/faq" />
                   <ListMenu name="Поддержать" href="/support" />
                   <ListMenu name="Онлайн карта" href="/online_map" />
                   <ListMenu name="Статистика" href="/statistic" />
                   <ListMenu name="Галерея" href="/gallery" />
-                  <ListMenu name="Калькулятор" href="https://wiki.gmgame.ru/" />
+                  <ListMenu name="Калькулятор" href="https://minecraft-calculator.netlify.app/" />
                   <ListMenu name="Вики сервера" href="https://wiki.gmgame.ru/" />
                   <ListMenu name="Торговая зона" href="/shopkeepers" />
                 </div>
@@ -149,32 +84,28 @@ const Header = () => {
           </div>
           <Swipe to="/faq" name="Вопросы" />
         </div>
-        <div className={classNames(styles["right_block"], styles["container_numbered"])}>
+        <div className={cN(styles["right_block"], styles["container_numbered"])}>
           {!resParams?.data?.discordUser ? (
             <Swipe to="/api/login" name="Войти" />
           ) : (
-            <div className={classNames(styles["profile_button"])} onClick={() => setCabDropMenu(!cabDropMenu)}>
+            <div className={styles["profile_button"]} onClick={() => setCabDropMenu((prev) => !prev)}>
               <DiscordIcon />
-              <label className={classNames(styles["name_user"])}>{resParams.data.discordUser.username}</label>
-              {cabDropMenu === false ? undefined : (
-                <div
-                  className={classNames(styles["dropdown_menu_profile"])}
-                  data-aos="zoom-out-down"
-                  data-aos-duration="250"
-                >
-                  <div className={classNames(styles["box_page_list"])}>
-                    <div className={classNames(styles["discord_name"])}>
+              <label className={styles["name_user"]}>{resParams.data.discordUser.username}</label>
+              {!cabDropMenu ? null : (
+                <div className={styles["dropdown_menu_profile"]} data-aos="zoom-out-down" data-aos-duration="250">
+                  <div className={styles["box_page_list"]}>
+                    <div className={styles["discord_name"]}>
                       <DiscordIcon />
-                      <label className={classNames(styles["string"])}>{resParams.data.discordUser.username}</label>
+                      <label className={styles["string"]}>{resParams.data.discordUser.username}</label>
                     </div>
-                    <div className={classNames(styles["br_line"])} />
+                    <div className={styles["br_line"]} />
                     {!resParams?.data?.user ? null : (
-                      <div className={classNames(styles["balance"])}>
-                        <span className={classNames(styles["icon_list"])}>
+                      <div className={styles["balance"]}>
+                        <span className={styles["icon_list"]}>
                           <CoinsSvgComponent width="100%" height="100%" color="#f4f4f4" />
                         </span>
-                        <span className={classNames(styles["text"])}>{resParams.data.user.balance} монет</span>
-                        <a className={classNames(styles["info"])} href="/">
+                        <span className={styles["text"]}>{resParams.data.user.balance} монет</span>
+                        <a className={styles["info"]} href="/">
                           ?
                         </a>
                       </div>
@@ -192,9 +123,9 @@ const Header = () => {
                       />
                     )}
                     <ListMenu
+                      button
                       name="Выйти из аккаунта"
                       onClick={logout}
-                      button={true}
                       icon={<GoOutSvgComponent width="100%" height="100%" color="#f4f4f4" />}
                     />
                   </div>
---
+++ src/common/header/useHeader.js
@@ -0,0 +1,64 @@
+import { useCallback, useEffect, useState } from "react";
+import { sendRequest, useAxios } from "../../DataProvider";
+import AOS from "aos";
+
+const useHeader = () => {
+  const resParams = useAxios("/api/profile", "GET", {});
+
+  const [cabDropMenu, setCabDropMenu] = useState(false);
+  const [mainDropMenu, setMainDropMenu] = useState(false);
+
+  const closeMenuMain = useCallback(() => {
+    setMainDropMenu(false);
+  }, []);
+
+  const closeMenuCab = useCallback(() => {
+    setCabDropMenu(false);
+  }, []);
+
+  const logout = () => {
+    sendRequest("/api/logout", "POST", {}).then(() => {
+      localStorage.clear();
+      window.location.href = "/";
+    });
+  };
+
+  useEffect(() => {
+    if (cabDropMenu) {
+      document.addEventListener("click", closeMenuCab);
+    } else {
+      document.removeEventListener("click", closeMenuCab);
+    }
+
+    return () => {
+      document.removeEventListener("click", closeMenuCab);
+    };
+  }, [cabDropMenu, closeMenuCab]);
+
+  useEffect(() => {
+    if (mainDropMenu) {
+      document.addEventListener("click", closeMenuMain);
+    } else {
+      document.removeEventListener("click", closeMenuMain);
+    }
+
+    return () => {
+      document.removeEventListener("click", closeMenuMain);
+    };
+  }, [closeMenuMain, mainDropMenu]);
+
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
+  return {
+    resParams,
+    cabDropMenu,
+    mainDropMenu,
+    logout,
+    setMainDropMenu,
+    setCabDropMenu,
+  };
+};
+
+export default useHeader;
