c198764b3d93f666711733a04b3f6d05c829ddc1
Yuriy Rusan <76946109+JuraRusan@users.noreply.github.com>
2025-08-09 22:15:28 +0300

    update general (#56)

--- src/modules/pages/general/General.js
+++ src/modules/pages/general/General.js
@@ -1,8 +1,7 @@
-import classNames from "classnames";
+import CN from "classnames";
 import React, { useEffect } from "react";
 import AOS from "aos";
 import { Link } from "react-router-dom";
-import World from "../../components/[0_grouped_0]-Generals/world/World.js";
 import HoverMeSvgComponent from "../../../bases/icons/hoverMeSvg/HoverMeSvg";
 import LogoMainTextSvgComponent from "../../../bases/icons/LogoMainText";
 
@@ -32,95 +31,117 @@ const INFORMATION_RES = "Здесь добываются все необходи
 const INFORMATION_GMGAME = "Мир со всеми постройками и проектами. Игроки выбирают территорию и занимаются строительством. Вайпов нет, но при обновлении генерации мир расширяют."; // prettier-ignore
 const INFORMATION_FARM = "Все механизмы и фермы с редстоуном строятся здесь, чтобы не нагружать основной сервер. Не вайпается и расширяется по необходимости."; // prettier-ignore
 
+const World = ({ name, src, inf }) => {
+  return (
+    <div className={styles["worlds"]}>
+      <div className={styles["circle"]} />
+      <h3 className={styles["name"]}>{name}</h3>
+      <div className={styles["hr_90_deg"]} />
+      <div className={styles["image_text"]}>
+        <img src={src} alt="" />
+        <h4 className={styles["information"]}>{inf}</h4>
+      </div>
+    </div>
+  );
+};
+
 const General = () => {
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
 
   return (
-    <div className={classNames(styles["main"])}>
-      <div className={classNames(styles["rowContainerNumberOne"])}>
-        <div className={classNames(styles["lineNoContentTop"])}></div>
-        <div className={classNames(styles["mainContent"])}>
-          {/*<h1 className={classNames(styles["mainTitleH1"])} data-aos="zoom-in">*/}
-          {/*  <span className={classNames(styles["coloredTitleSpanOne"])}>G</span>*/}
-          {/*  <span className={classNames(styles["coloredTitleSpanTwo"])}>M</span>GAME*/}
-          {/*</h1>*/}
-          <div className={classNames(styles["logo"])}>
+    <div className={styles["main"]}>
+      <div className={styles["row_container_one"]}>
+        <div className={styles["line_no_content_top"]}></div>
+        <div className={styles["main_content"]}>
+          <div className={styles["logo"]}>
             <LogoMainTextSvgComponent height="100%" width="100%" />
           </div>
-          <h3 className={classNames(styles["mainTextH3"])} data-aos="zoom-in">
+          <h3 className={styles["main_text"]} data-aos="zoom-in">
             {INFORMATION_1}
           </h3>
-          <h3 className={classNames(styles["mainTextH3"])} data-aos="zoom-in">
+          <h3 className={styles["main_text"]} data-aos="zoom-in">
             {INFORMATION_2}
           </h3>
-          <div className={classNames(styles["buttonContainerWrapper"])} data-aos="zoom-in">
+          <div className={styles["button_application"]} data-aos="zoom-in">
             <Link to={"/cab/profile"}>
-              <button className={classNames(styles["btn"])}>Подать заявку</button>
+              <button className={styles["btn"]}>Подать заявку</button>
             </Link>
           </div>
         </div>
-        <div className={classNames(styles["lineNoContentBottom"])}></div>
+        <div className={styles["line_no_content_bottom"]}></div>
       </div>
 
-      <div className={classNames(styles["rowContainerNumberTwo"])}>
-        <div className={classNames(styles["contentInformation"])} data-aos="zoom-in">
-          <div className={classNames(styles["informationTextDivStyle"])}>
-            <h2 className={classNames(styles["h2Information"], styles["leftH2"])}>{TITLE_CONCEPT}</h2>
-            <h4 className={classNames(styles["h4Information"], styles["leftH4"])}>{CONCEPT}</h4>
+      <div className={styles["row_container_two"]}>
+        <div className={styles["content_information"]} data-aos="zoom-in">
+          <div className={styles["information_text_div_style"]}>
+            <h2 className={CN(styles["h2_information"], styles["left_h2"])}>{TITLE_CONCEPT}</h2>
+            <h4 className={CN(styles["h4_information"], styles["left_h4"])}>{CONCEPT}</h4>
           </div>
-          <div className={classNames(styles["informationWrapperImage"])}>
+          <div className={styles["information_wrapper_image"]}>
             <img
-              className={classNames(styles["informationImageStyle"])}
-              src="./site_assets/pages/webp/new_1.webp"
+              className={styles["information_image_style"]}
+              src={process.env.PUBLIC_URL + "./site_assets/pages/webp/new_1.webp"}
               alt=""
             />
           </div>
         </div>
-        <div className={classNames(styles["contentInformation"], styles["reverse"])} data-aos="zoom-in">
-          <div className={classNames(styles["informationWrapperImage"])}>
+        <div className={CN(styles["content_information"], styles["reverse"])} data-aos="zoom-in">
+          <div className={styles["information_wrapper_image"]}>
             <img
-              className={classNames(styles["informationImageStyle"])}
-              src="./site_assets/pages/webp/new_2.webp"
+              className={styles["information_image_style"]}
+              src={process.env.PUBLIC_URL + "./site_assets/pages/webp/new_2.webp"}
               alt=""
             />
           </div>
-          <div className={classNames(styles["informationTextDivStyle"])}>
-            <h2 className={classNames(styles["h2Information"], styles["rightH2"])}>{TITLE_COMMUNITY}</h2>
-            <h4 className={classNames(styles["h4Information"], styles["rightH4"])}>{COMMUNITY}</h4>
+          <div className={styles["information_text_div_style"]}>
+            <h2 className={CN(styles["h2_information"], styles["right_h2"])}>{TITLE_COMMUNITY}</h2>
+            <h4 className={CN(styles["h4_information"], styles["right_h4"])}>{COMMUNITY}</h4>
           </div>
         </div>
-        <div className={classNames(styles["contentInformation"])} data-aos="zoom-in">
-          <div className={classNames(styles["informationTextDivStyle"])}>
-            <h2 className={classNames(styles["h2Information"], styles["leftH2"])}>{TITLE_GAME_PROCESS}</h2>
-            <h4 className={classNames(styles["h4Information"], styles["leftH4"])}>{GAME_PROCESS}</h4>
+        <div className={styles["content_information"]} data-aos="zoom-in">
+          <div className={styles["information_text_div_style"]}>
+            <h2 className={CN(styles["h2_information"], styles["left_h2"])}>{TITLE_GAME_PROCESS}</h2>
+            <h4 className={CN(styles["h4_information"], styles["left_h4"])}>{GAME_PROCESS}</h4>
           </div>
-          <div className={classNames(styles["informationWrapperImage"])}>
+          <div className={styles["information_wrapper_image"]}>
             <img
-              className={classNames(styles["informationImageStyle"])}
-              src="./site_assets/pages/webp/new_3.webp"
+              className={styles["information_image_style"]}
+              src={process.env.PUBLIC_URL + "./site_assets/pages/webp/new_3.webp"}
               alt=""
             />
           </div>
         </div>
       </div>
 
-      <div className={classNames(styles["rowContainerNumberThree"])}>
-        <div className={classNames(styles["lineNoContentTop"])}></div>
-        <div className={classNames(styles["titleBlock"])} data-aos="zoom-in">
-          <h2 className={classNames(styles["titleH3"])}>{DIVISION_INTO_WORLDS}</h2>
-          <h4 className={classNames(styles["titleH4"])}>{INFORMATION_TPS}</h4>
+      <div className={styles["row_container_three"]}>
+        <div className={styles["line_no_content_top"]}></div>
+        <div className={styles["title_block"]} data-aos="zoom-in">
+          <h2 className={styles["title_h3"]}>{DIVISION_INTO_WORLDS}</h2>
+          <h4 className={styles["title_h4"]}>{INFORMATION_TPS}</h4>
         </div>
-        <div className={classNames(styles["blockWorlds"])} data-aos="zoom-in">
-          <div className={classNames(styles["hoverMeBlock"])}>
+        <div className={styles["block_worlds"]} data-aos="zoom-in">
+          <div className={styles["hove_me"]}>
             <HoverMeSvgComponent width="100%" height="100%" color="#292929" />
           </div>
-          <World name={NAME_RES} src="./site_assets/pages/webp/new_4.webp" inf={INFORMATION_RES} />
-          <World name={NAME_GMGAME} src="./site_assets/pages/webp/new_5.webp" inf={INFORMATION_GMGAME} />
-          <World name={NAME_FARM} src="./site_assets/pages/webp/new_6.webp" inf={INFORMATION_FARM} />
+          <World
+            name={NAME_RES}
+            src={process.env.PUBLIC_URL + "./site_assets/pages/webp/new_4.webp"}
+            inf={INFORMATION_RES}
+          />
+          <World
+            name={NAME_GMGAME}
+            src={process.env.PUBLIC_URL + "./site_assets/pages/webp/new_5.webp"}
+            inf={INFORMATION_GMGAME}
+          />
+          <World
+            name={NAME_FARM}
+            src={process.env.PUBLIC_URL + "./site_assets/pages/webp/new_6.webp"}
+            inf={INFORMATION_FARM}
+          />
         </div>
-        <div className={classNames(styles["lineNoContentBottom"])}></div>
+        <div className={styles["line_no_content_bottom"]} />
       </div>
     </div>
   );
