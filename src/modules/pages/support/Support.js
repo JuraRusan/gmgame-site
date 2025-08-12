6767c47519a3ed8d8f94fc430d35072c712c1cf1
Yuriy Rusan <76946109+JuraRusan@users.noreply.github.com>
2025-08-12 09:08:10 +0300

    update support (#60)

--- src/modules/pages/support/Support.js
+++ src/modules/pages/support/Support.js
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import React from "react";
 import useLoading from "../../loading/useLoading";
 import Preload from "../../components/preloader/Preload";
@@ -8,8 +7,18 @@ import Title from "../../components/title/Title";
 import styles from "./Support.module.scss";
 
 const LINKS = [
-  { description: "Подарить алмазы (HotMC)", link: "https://hotmc.ru/promote-minecraft-server-205185?mode=packs" },
-  { description: "Проголосовать на мониторингах", link: "https://discord.gg/AesVsdPsFj" },
+  {
+    description: "Подарить алмазы (HotMC)",
+    link: "https://hotmc.ru/promote-minecraft-server-205185?mode=packs",
+  },
+  {
+    description: "Подарить промо баллы на (MinecraftRating)",
+    link: "https://minecraftrating.ru/promote/290714/#buy-rating",
+  },
+  {
+    description: "Проголосовать на мониторингах",
+    link: "https://discord.com/channels/723912565234728972/994605541135491183/1356505401134416016",
+  },
 ];
 
 const MONITORING = [
@@ -38,6 +47,11 @@ const MONITORING = [
     name: "MCLike",
     url: "https://mclike.com/minecraft-server-192962",
   },
+  {
+    id: 5,
+    name: "Planet Minecraft",
+    url: "https://www.planetminecraft.com/server/gmgame/",
+  },
 ];
 
 const Support = () => {
@@ -48,42 +62,46 @@ const Support = () => {
   }
 
   return (
-    <div className={classNames(styles["main_support_block"])}>
-      <div className={classNames(styles["support"])}>
+    <div className={styles["main_support_block"]}>
+      <div className={styles["support"]}>
         <Title>Поддержать проект</Title>
-        <div className={classNames(styles["information"])}>
-          <div className={classNames(styles["text_wrapper"])}>
-            <p className={classNames(styles["text_line"])}>
-              Сервер GMGame работает уже больше двух лет. За это время на сервере были построены разнообразные и
+        <div className={styles["information"]}>
+          <div className={styles["text_wrapper"]}>
+            <p className={styles["text_line"]}>
+              Сервер GMGame работает уже больше четырёх лет. За это время на сервере были построены разнообразные и
               масштабные проекты, проведены различные ивенты и праздники, сервер приобрел множество друзей, соратников и
               подарил огромное количество эмоций разным людям.
             </p>
-            <p className={classNames(styles["text_line"])}>
+            <p className={styles["text_line"]}>
               Благодаря этим людям сервер живет и позволяет комфортно играть без лагов и читеров. Мы стараемся каждый
               день улучшить игровой опыт на сервере и будем признательны за любую помощь. Если у вас есть возможность
               помочь серверу нижеперечисленными способами, то, пожалуйста, воспользуйтесь ими.
             </p>
-            <p className={classNames(styles["text_line"])}>
-              На пожертвования уже куплено мощное железо для сервера, источник бесперебойного питания и множество
-              плагинов для комфортной игры. Спасибо всем игрокам, без вашей поддержки не было бы сервера GMGame.
+            <p className={styles["text_line"]}>
+              На пожертвования уже куплено{" "}
+              <Link
+                target="_blank"
+                rel="noreferrer"
+                show={false}
+                href="https://wiki.gmgame.ru/books/server/page/konfigi-servera"
+              >
+                мощное железо
+              </Link>{" "}
+              для сервера, источник бесперебойного питания и множество плагинов для комфортной игры. Спасибо всем
+              игрокам, без вашей поддержки не было бы сервера GMGame.
             </p>
-            <div className={classNames(styles["link_wrapper"])}>
+            <div className={styles["link_wrapper"]}>
               {LINKS.map((el, i) => (
-                <Link
-                  target="_blank"
-                  rel="noreferrer"
-                  href={el.link}
-                  className={classNames(styles["link_support_custom"])}
-                >
+                <Link target="_blank" rel="noreferrer" href={el.link} className={styles["link_support_custom"]} key={i}>
                   &#8226; {el.description}
                 </Link>
               ))}
             </div>
-            <p className={classNames(styles["text_line"])}>Спасибо! Ваша поддержка очень важна. &#129392;</p>
+            <p className={styles["text_line"]}>Спасибо! Ваша поддержка очень важна. &#129392;</p>
           </div>
-          <div className={classNames(styles["img_wrapper"])}>
+          <div className={styles["img_wrapper"]}>
             <img
-              className={classNames(styles["image"])}
+              className={styles["image"]}
               src={process.env.PUBLIC_URL + "/site_assets/pages/webp/new_7.webp"}
               width="100%"
               height="100%"
@@ -91,10 +109,10 @@ const Support = () => {
             />
           </div>
         </div>
-        <div className={classNames(styles["monitoring_block"])}>
-          <h4 className={classNames(styles["sub_title"])}>Мы на мониторингах</h4>
+        <div className={styles["monitoring_block"]}>
+          <h4 className={styles["sub_title"]}>Мы на мониторингах</h4>
           {MONITORING.map((el, i) => (
-            <a className={classNames(styles["monitoring_card"])} target="_blank" rel="noreferrer" href={el.url} key={i}>
+            <a className={styles["monitoring_card"]} target="_blank" rel="noreferrer" href={el.url} key={i}>
               {el.name}
             </a>
           ))}
