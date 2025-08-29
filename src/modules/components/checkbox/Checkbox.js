60eddd6923814ec89422f77056baf07baaf9dca7
JuraRusan <rusanjura@gmail.com>
2025-08-29 23:08:21 +0300

    Checkbox.js use forwardRef

--- src/modules/components/checkbox/Checkbox.js
+++ src/modules/components/checkbox/Checkbox.js
@@ -1,23 +1,25 @@
-import classNames from "classnames";
-import React from "react";
+import CN from "classnames";
+import React, { forwardRef } from "react";
 
 import styles from "./Checkbox.module.scss";
 
-const Checkbox = ({ checked, onChange, onClick, message, className, props }) => {
-  const handleChange = () => {
-    onChange(!checked);
+const Checkbox = forwardRef(({ checked, onChange, onClick, message, className, ...props }, ref) => {
+  const handleChange = (e) => {
+    if (onChange) {
+      onChange(e);
+    }
     if (onClick) {
-      onClick(!checked);
+      onClick(e.target.checked);
     }
   };
 
   return (
-    <label className={classNames(styles["wrapper"], className)}>
-      <input type="checkbox" checked={checked} onChange={handleChange} {...props} />
-      <span className={classNames(styles["checkmark"])} />
-      <span className={classNames(styles["message"])}>{message}</span>
+    <label className={CN(styles["wrapper"], className)}>
+      <input type="checkbox" ref={ref} checked={checked} onChange={handleChange} {...props} />
+      <span className={styles["checkmark"]} />
+      <span className={styles["message"]}>{message}</span>
     </label>
   );
-};
+});
 
 export default Checkbox;
