b00b86200d8efbeaaff225ab03a4d1673e7af102
JuraRusan <rusanjura@gmail.com>
2025-08-29 23:21:34 +0300

    Textarea.js теперь использует ограничение высоты

--- src/modules/components/auth-comp/Auth-comp.js
+++ src/modules/components/auth-comp/Auth-comp.js
@@ -133,15 +133,15 @@ const AuthComponent = () => {
           <Input type="text" autoComplete="off" placeholder="&nbsp;" {...formFields["friend_name"]} />
           {/* --- */}
           <FormTitle title="Откуда узнали о проекте:" min={24} max={255} length={watch("about")?.length || 0} />
-          <Textarea {...formFields["about"]} />
+          <Textarea rows="3" {...formFields["about"]} max_height="large" />
           <ErrorRender name="about" />
           {/* --- */}
           <FormTitle title="Интересы в майнкрафте:" min={32} max={1000} length={watch("interests")?.length || 0} />
-          <Textarea {...formFields["interests"]} />
+          <Textarea rows="3" {...formFields["interests"]} max_height="large" />
           <ErrorRender name="interests" />
           {/* --- */}
           <FormTitle title="Предыдущие сервера:" min={12} max={255} length={watch("back_servers")?.length || 0} />
-          <Textarea {...formFields["back_servers"]} style={{ maxHeight: "250px" }} />
+          <Textarea {...formFields["back_servers"]} rows="3" max_height="large" />
           <ErrorRender name="back_servers" />
           {/* --- */}
           <Checkbox message="Да, я прочитал правила и обязуюсь им следовать" {...formFields["checkbox"]} />
@@ -151,7 +151,6 @@ const AuthComponent = () => {
         </div>
         <form className={styles["action_block"]}>
           <Button
-            id="submitButton"
             type="submit"
             label="Отправить"
             view="submit"
--- src/modules/components/textarea/Textarea.js
+++ src/modules/components/textarea/Textarea.js
@@ -3,8 +3,22 @@ import React, { forwardRef } from "react";
 
 import styles from "./Textarea.module.scss";
 
-const Textarea = forwardRef(({ className, ...props }, ref) => {
-  return <textarea ref={ref} className={CN(styles["textarea"], className)} {...props} />;
+const Textarea = forwardRef(({ max_height = "small", className, ...props }, ref) => {
+  return (
+    <textarea
+      ref={ref}
+      className={CN(
+        styles["textarea"],
+        {
+          [styles["height_small"]]: max_height === "small",
+          [styles["height_medium"]]: max_height === "medium",
+          [styles["height_large"]]: max_height === "large",
+        },
+        className
+      )}
+      {...props}
+    />
+  );
 });
 
 export default Textarea;
