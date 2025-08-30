0191389ede616124b898381dcbe93f49d78e41df
JuraRusan <rusanjura@gmail.com>
2025-08-30 20:24:32 +0300

    new package

--- src/modules/components/auth-comp/Auth-comp.js
+++ src/modules/components/auth-comp/Auth-comp.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
 import { sendRequest } from "../../../DataProvider";
@@ -11,6 +11,8 @@ import Input from "../input/Input";
 import Select from "../select/Select";
 import Textarea from "../textarea/Textarea";
 import Checkbox from "../checkbox/Checkbox";
+import ReactSkinview3d from "react-skinview3d";
+import * as skinview3d from "skinview3d";
 
 import styles from "./Auth-comp.module.scss";
 
@@ -19,8 +21,20 @@ const AKK_VALUE = [
   { value: "0", name: "Пиратка" },
 ];
 
+const WatchText = ({ text, watch }) => {
+  return (
+    <p className={styles["display_watch"]}>
+      {text}{" "}
+      <span className={styles["colored"]}>{watch && watch.length > 120 ? watch.substring(0, 120) + "..." : watch}</span>
+    </p>
+  );
+};
+
 const AuthComponent = () => {
   const alert = useAlert();
+
+  const [outputSkin, setOutputSkin] = useState("https://mineskin.eu/skin/steve");
+  const [outputTypeAkk, setOutputTypeAkk] = useState("");
 
   const {
     register,
@@ -60,6 +74,21 @@ const AuthComponent = () => {
       }
     });
   };
+
+  useEffect(() => {
+    const subscription = watch((data) => {
+      let username = "steve";
+
+      if (data.username && data.username.length < 17) {
+        username = data.username;
+      }
+
+      setOutputSkin("https://mineskin.eu/skin/" + username);
+      setOutputTypeAkk(data.type_account === "1" ? "лицензия" : "пиратка");
+    });
+
+    return () => subscription.unsubscribe();
+  }, [watch, setOutputSkin, setOutputTypeAkk]);
 
   const formFields = useMemo(() => {
     return {
@@ -112,55 +141,84 @@ const AuthComponent = () => {
     <div className={styles["application_block"]}>
       <Title>Создание заявки на GMGame</Title>
       <div className={styles["container"]}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {/* --- */}
-          <FormTitle title="Игровой ник:" min={3} max={16} length={watch("username")?.length || 0} />
-          <Input type="text" autoComplete="off" placeholder="&nbsp;" {...formFields["username"]} />
-          <ErrorRender name="username" />
-          {/* --- */}
-          <FormTitle title="Пароль для входа на сервер:" min={8} max={32} length={watch("password")?.length || 0} />
-          <Input type="password" autoComplete="off" placeholder="&nbsp;" {...formFields["password"]} />
-          <ErrorRender name="password" />
-          {/* --- */}
-          <FormTitle title="Тип аккаунта:" count={false} />
-          <Select list={AKK_VALUE} {...formFields["type_account"]} />
-          <ErrorRender name="type_account" />
-          {/* --- */}
-          <FormTitle title="Возраст:" count={false} />
-          <Input type="number" {...formFields["age"]} />
-          <ErrorRender name="age" />
-          {/* --- */}
-          {/* prettier-ignore */}
-          <FormTitle title="Ник друга, если играете с кем-то:" required={false} min={0} max={255} length={watch("friend_name")?.length || 0}/>
-          <Input type="text" autoComplete="off" placeholder="&nbsp;" {...formFields["friend_name"]} />
-          {/* --- */}
-          <FormTitle title="Откуда узнали о проекте:" min={24} max={255} length={watch("about")?.length || 0} />
-          <Textarea rows="3" max_height="large" {...formFields["about"]} />
-          <ErrorRender name="about" />
-          {/* --- */}
-          <FormTitle title="Интересы в майнкрафте:" min={32} max={1000} length={watch("interests")?.length || 0} />
-          <Textarea rows="3" max_height="large" {...formFields["interests"]} />
-          <ErrorRender name="interests" />
-          {/* --- */}
-          <FormTitle title="Предыдущие сервера:" min={12} max={255} length={watch("back_servers")?.length || 0} />
-          <Textarea rows="3" max_height="large" {...formFields["back_servers"]} />
-          <ErrorRender name="back_servers" />
-          {/* --- */}
-          {/* prettier-ignore */}
-          <Checkbox className={styles["check"]} message="Да, я прочитал правила и обязуюсь им следовать" {...formFields["checkbox"]} />
-        </form>
-        <div className={styles["wrapper_warn"]}>
-          <Notifications inf="Относитесь ответственно к заполнению заявки" type="warn" />
+        <div className={styles["main_form"]}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {/* --- */}
+            <FormTitle title="Игровой ник:" min={3} max={16} length={watch("username")?.length || 0} />
+            <Input type="text" autoComplete="off" placeholder="&nbsp;" {...formFields["username"]} />
+            <ErrorRender name="username" />
+            {/* --- */}
+            <FormTitle title="Пароль для входа на сервер:" min={8} max={32} length={watch("password")?.length || 0} />
+            <Input type="password" autoComplete="off" placeholder="&nbsp;" {...formFields["password"]} />
+            <ErrorRender name="password" />
+            {/* --- */}
+            <FormTitle title="Тип аккаунта:" count={false} />
+            <Select list={AKK_VALUE} {...formFields["type_account"]} />
+            <ErrorRender name="type_account" />
+            {/* --- */}
+            <FormTitle title="Возраст:" count={false} />
+            <Input type="number" {...formFields["age"]} />
+            <ErrorRender name="age" />
+            {/* --- */}
+            {/* prettier-ignore */}
+            <FormTitle title="Ник друга, если играете с кем-то:" required={false} min={0} max={255} length={watch("friend_name")?.length || 0}/>
+            <Input type="text" autoComplete="off" placeholder="&nbsp;" {...formFields["friend_name"]} />
+            {/* --- */}
+            <FormTitle title="Откуда узнали о проекте:" min={24} max={255} length={watch("about")?.length || 0} />
+            <Textarea rows="3" max_height="large" {...formFields["about"]} />
+            <ErrorRender name="about" />
+            {/* --- */}
+            <FormTitle title="Интересы в майнкрафте:" min={32} max={1000} length={watch("interests")?.length || 0} />
+            <Textarea rows="3" max_height="large" {...formFields["interests"]} />
+            <ErrorRender name="interests" />
+            {/* --- */}
+            <FormTitle title="Предыдущие сервера:" min={12} max={255} length={watch("back_servers")?.length || 0} />
+            <Textarea rows="3" max_height="large" {...formFields["back_servers"]} />
+            <ErrorRender name="back_servers" />
+            {/* --- */}
+            {/* prettier-ignore */}
+            <Checkbox className={styles["check"]} message="Да, я прочитал правила и обязуюсь им следовать" {...formFields["checkbox"]} />
+          </form>
+          <div className={styles["wrapper_warn"]}>
+            <Notifications inf="Относитесь ответственно к заполнению заявки" type="warn" />
+          </div>
         </div>
-        <form className={styles["action_block"]}>
-          <Button
-            type="submit"
-            label="Отправить"
-            view="submit"
-            disabled={!isValid}
-            onClick={handleSubmit(registration)}
-          />
-        </form>
+        <div className={styles["check_form"]}>
+          <div className={styles["card_wrapper"]}>
+            <div className={styles["main_watch"]}>
+              <ReactSkinview3d
+                skinUrl={outputSkin}
+                height={300}
+                width={150}
+                onReady={({ viewer }) => {
+                  viewer.animation = new skinview3d.IdleAnimation();
+                  viewer.animation.speed = 1;
+                }}
+                options={{ zoom: 0.9 }}
+              />
+              <div className={styles["main_info"]}>
+                <WatchText text="Мой ник в игре:" watch={watch("username")} />
+                <WatchText text="Мой возраст:" watch={watch("age")} />
+                <WatchText text="Тип аккаунта:" watch={outputTypeAkk} />
+                <WatchText text="Ник друга с которым я играю:" watch={watch("friend_name")} />
+              </div>
+            </div>
+            <div className={styles["additional_watch"]}>
+              <WatchText text="Я узнал о проекте:" watch={watch("about")} />
+              <WatchText text="Интересы в майнкрафте:" watch={watch("interests")} />
+              <WatchText text="Предыдущие сервера:" watch={watch("back_servers")} />
+            </div>
+          </div>
+          <div className={styles["action_block"]}>
+            <Button
+              type="submit"
+              label="Отправить"
+              view="submit"
+              disabled={!isValid}
+              onClick={handleSubmit(registration)}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
