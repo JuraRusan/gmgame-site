a1b749eb1e20714fb2a44f8cd95d6d8a4d8bf708
JuraRusan <rusanjura@gmail.com>
2025-08-30 13:33:51 +0300

    select custom arrow style

--- src/modules/components/select/Select.js
+++ src/modules/components/select/Select.js
@@ -5,14 +5,17 @@ import styles from "./Select.module.scss";
 
 const Select = forwardRef(({ list, className, ...props }, ref) => {
   return (
-    <select ref={ref} defaultValue="" className={CN(styles["select"], className)} {...props}>
-      <option hidden value="" />
-      {list.map((el, index) => (
-        <option className={styles["options"]} value={el.value} key={index}>
-          {el.name}
-        </option>
-      ))}
-    </select>
+    <div className={styles["select_customs"]}>
+      <select ref={ref} defaultValue="" className={CN(styles["select"], className)} {...props}>
+        <option hidden value="" />
+        {list.map((el, index) => (
+          <option className={styles["options"]} value={el.value} key={index}>
+            {el.name}
+          </option>
+        ))}
+      </select>
+      <div className={styles["swipe"]}>▼</div>
+    </div>
   );
 });
 
