036867684a82b1e4f177ec4f1bb8da0f9cb38133
JuraRusan <rusanjura@gmail.com>
2025-08-30 13:33:24 +0300

    input type password custom spin

--- src/modules/components/input/Input.js
+++ src/modules/components/input/Input.js
@@ -1,22 +1,36 @@
 import CN from "classnames";
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
 
 import styles from "./Input.module.scss";
 
 const CustomNumber = () => {
   return (
-    <div className={styles["number_block"]}>
+    <div className={CN(styles["custom_block"], styles["_number"])}>
       <div className={styles["swipe"]}>▲</div>
       <div className={styles["swipe"]}>▼</div>
     </div>
   );
 };
 
-const Input = forwardRef(({ className, ...props }, ref) => {
+const CustomPassword = ({ onToggle, visible }) => {
+  return (
+    <label className={CN(styles["custom_block"], styles["_password"])}>
+      <input className={styles["no_checkmark"]} type="checkbox" checked={visible} onChange={onToggle} />
+      <span className={styles["checkmark"]} />
+    </label>
+  );
+};
+
+const Input = forwardRef(({ type, className, ...props }, ref) => {
+  const [visible, setVisible] = useState(false);
+
+  const inputType = type === "password" ? (visible ? "text" : "password") : type;
+
   return (
     <div className={styles["input_customs"]}>
-      <input ref={ref} className={CN(styles["input"], className)} {...props} />
-      {props.type !== "number" ? null : <CustomNumber />}
+      <input ref={ref} type={inputType} className={CN(styles["input"], className)} {...props} />
+      {type !== "number" ? null : <CustomNumber />}
+      {type !== "password" ? null : <CustomPassword visible={visible} onToggle={() => setVisible(!visible)} />}
     </div>
   );
 });
