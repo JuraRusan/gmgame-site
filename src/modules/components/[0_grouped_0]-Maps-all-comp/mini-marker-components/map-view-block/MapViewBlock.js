13fa4fe48647e43c2d74491f21eef3ac80d20198
JuraRusan <rusanjura@gmail.com>
2025-09-19 10:38:27 +0300

    ренейм

--- src/modules/components/[0_grouped_0]-Maps-all-comp/mini-marker-components/map-view-block/MapViewBlock.js
+++ src/modules/components/[0_grouped_0]-Maps-all-comp/mini-marker-components/map-view-block/MapViewBlock.js
@@ -13,11 +13,11 @@ const MapViewBlock = ({ link_to, index, name, onClick }) => {
     <div className={styles["one_maps_element"]}>
       <Link to={link_to} className={styles["link_to"]}>
         <div className={styles["wrapper"]}>
-          <p className={styles["elementIndex"]}>{index + 1}</p>
-          <h3 className={styles["elementH3Name"]}>{name}</h3>
+          <p className={styles["index_count"]}>{index + 1}</p>
+          <h3 className={styles["element_name"]}>{name}</h3>
         </div>
       </Link>
-      <div className={styles["action_column"]}>
+      <div className={styles["action_box"]}>
         <ActionsButton onClick={() => setIsConfirmActive(true)} ico={<BinSvgComponent width="100%" height="100%" />} />
         <ConfirmModal
           open={isConfirmActive}
