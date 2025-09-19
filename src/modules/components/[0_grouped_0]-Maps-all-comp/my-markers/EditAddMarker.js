95db8a2c12c317f159a0f3f122c49f365eb6c598
JuraRusan <rusanjura@gmail.com>
2025-09-19 15:02:19 +0300

    useForm для меток

--- src/modules/components/[0_grouped_0]-Maps-all-comp/my-markers/EditAddMarker.js
+++ src/modules/components/[0_grouped_0]-Maps-all-comp/my-markers/EditAddMarker.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Notifications from "../../notifications/Notifications";
 import { sendRequest, useAxios } from "../../../../DataProvider";
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useAlert } from "@blaumaus/react-alert";
 import Preload from "../../preloader/Preload";
 import useLoading from "../../../loading/useLoading";
 import Button from "../../button/Button";
-import { checkName } from "../mini-marker-components/function/CheckName";
-import { checkDescription } from "../mini-marker-components/function/CheckDescription";
-import { checkCoordinates } from "../mini-marker-components/function/CheckCoordinates";
 import BackButton from "../../back-button/BackButton";
 import ConfirmModal from "../../confirm-modal/ConfirmModal";
 import Input from "../../input/Input";
 import Textarea from "../../textarea/Textarea";
 import Select from "../../select/Select";
 import FormTitle from "../../form-title/FormTitle";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 import styles from "../maps-elements-add.module.scss";
 
@@ -23,7 +22,7 @@ const WORLD_VALUE = [
   { value: "farm", name: "Фермерский мир" },
 ];
 
-const VALUE_OPTION = [
+const OPTION_VALUE = [
   { value: "basePlayers", name: "Базы игроков" },
   { value: "city", name: "Города" },
   { value: "shopping_centers", name: "Торговые центры - over" },
@@ -36,144 +35,113 @@ const VALUE_OPTION = [
   { value: "pixel_arts", name: "Пиксель арты - end" },
 ];
 
-const MIN_NAME = 12;
-const MAX_NAME = 255;
-const MIN_DESCRIPTION = 24;
-const MAX_DESCRIPTION = 255;
+const ErrorRender = ({ name, errors }) => {
+  return (
+    <ErrorMessage errors={errors} name={name} render={({ message }) => <Notifications inf={message} type="error" />} />
+  );
+};
 
-const EditAddMarker = (params) => {
+const EditAddMarker = () => {
   const isLoading = useLoading();
-
-  const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
   const alert = useAlert();
 
+  const navigate = useNavigate();
+  const location = useLocation();
   const { id } = useParams();
 
+  console.log(location);
+
   const [isConfirmActive, setIsConfirmActive] = useState(false);
-
-  const [formName, setFormName] = useState("");
-  const [errorName, setErrorName] = useState("");
-
-  const [formDescription, setFormDescription] = useState("");
-  const [errorDescription, setErrorDescription] = useState("");
-
-  const [formServer, setFormServer] = useState("");
-  const [formType, setFormType] = useState("");
-
-  const [formX, setFormX] = useState(0);
-  const [formZ, setFormZ] = useState(0);
-  const [errorCoordinates, setErrorCoordinates] = useState("");
-
   const [init, setInit] = useState(false);
+
+  const [markerId, setMarkerId] = useState(id === "new" ? -1 : id);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      name: "",
+      description: "",
+      server: "",
+      type: "",
+      cordX: 0,
+      cordZ: 0,
+      id: markerId,
+    },
+  });
+
+  const resParams = useAxios(`/api/get_marker/${id}`, "GET", {});
+
+  const editMarker = (data) => {
+    markerRequest({ req: "edit", url: "/api/edit_marker", data });
+  };
+
+  const addMarker = (data) => {
+    markerRequest({ req: "create", url: "/api/add_marker", data });
+  };
+
+  const deleteMarker = (data) => {
+    markerRequest({ req: "delete", url: "/api/delete_marker", data });
+  };
+
+  const markerRequest = ({ req, url, data }) => {
+    sendRequest(url, "POST", {
+      name: data.name,
+      description: data.description,
+      server: data.server,
+      id_type: data.type,
+      x: data.cordX,
+      z: data.cordZ,
+      markerID: data.id,
+    }).then((response) => {
+      showMessage(response);
+
+      if (req === "delete") navigate(-1);
+      if (req === "create") navigate(`${location.pathname.replace("new", response.id)}`);
+    });
+  };
 
   const showMessage = (response) => {
     if (response.message) {
       alert.success(response.message);
-      navigate(-1);
     } else {
       alert.error(response.error);
     }
   };
 
-  const markerRequest = (url) => {
-    if (formName.length === 0) {
-      alert.error("Укажите название");
-      return;
-    }
-
-    if (errorName !== "") {
-      alert.error(errorName);
-      return;
-    }
-
-    if (formDescription.length === 0) {
-      alert.error("Добавте описание");
-      return;
-    }
-
-    if (errorDescription !== "") {
-      alert.error(errorDescription);
-      return;
-    }
-
-    if (formServer === "") {
-      alert.error("Выберите сервер");
-      return;
-    }
-
-    if (formType === "") {
-      alert.error("Выберите тип метки");
-      return;
-    }
-
-    if (errorCoordinates !== "") {
-      alert.error(errorCoordinates);
-      return;
-    }
-
-    if (formX === 0 && formZ === 0) {
-      alert.error("Укажите координаты");
-      return;
-    }
-
-    sendRequest(url, "POST", {
-      server: formServer,
-      id_type: formType,
-      name: formName,
-      x: formX,
-      z: formZ,
-      description: formDescription || "",
-      markerID: id === "new" ? -1 : id,
-    }).then((response) => {
-      showMessage(response);
-    });
-  };
-
-  const saveMarker = () => {
-    markerRequest("/api/edit_marker");
-  };
-
-  const addMarker = () => {
-    markerRequest("/api/add_marker");
-  };
-
-  const deleteMarker = () => {
-    markerRequest("/api/delete_marker");
-  };
-
-  const resParams = useAxios(`/api/get_marker/${id}`, "GET", {});
-
-  useEffect(() => {
-    if (!init) {
-      if (searchParams.get("_x")) {
-        const x = searchParams.get("_x");
-        setFormX(+x);
-        checkCoordinates(x, setErrorCoordinates);
-      }
-      if (searchParams.get("_z")) {
-        const z = searchParams.get("_z");
-        setFormZ(+z);
-        checkCoordinates(z, setErrorCoordinates);
-      }
-      if (searchParams.get("_server")) {
-        setFormServer(searchParams.get("_server"));
-      }
-      if (searchParams.get("_type")) {
-        setFormType(searchParams.get("_type"));
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    searchParams.set("_x", formX);
-    searchParams.set("_z", formZ);
-    searchParams.set("_server", formServer);
-    searchParams.set("_type", formType);
-
-    navigate({ search: searchParams.toString() }, { replace: true });
-  }, [formX, formZ, formServer, formType, searchParams, navigate]);
+  const formFields = useMemo(() => {
+    return {
+      name: register("name", {
+        required: { value: true, message: "Обязательное поле" },
+        minLength: { value: 12, message: "Слишком короткое название" },
+        maxLength: { value: 255, message: "Слишком длинное название" },
+      }),
+      description: register("description", {
+        required: { value: true, message: "Обязательное поле" },
+        minLength: { value: 24, message: "Слишком короткое описание" },
+        maxLength: { value: 255, message: "Слишком длинное описание" },
+      }),
+      server: register("server", {
+        required: { value: true, message: "Обязательное поле" },
+      }),
+      type: register("type", {
+        required: { value: true, message: "Обязательное поле" },
+      }),
+      cordX: register("cordX", {
+        required: { value: true, message: "Обязательное поле" },
+        valueAsNumber: true,
+      }),
+      cordZ: register("cordZ", {
+        required: { value: true, message: "Обязательное поле" },
+        valueAsNumber: true,
+      }),
+    };
+  }, [register]);
 
   if (resParams.loading || isLoading) {
     return <Preload full={false} />;
@@ -181,94 +149,70 @@ const EditAddMarker = (params) => {
 
   if (resParams.loaded && id !== "new" && !init) {
     setInit(true);
-    setFormType(resParams.data.marker?.id_type);
-    setFormDescription(resParams.data.marker?.description);
-    setFormX(resParams.data.marker?.x);
-    setFormZ(resParams.data.marker?.z);
-    setFormName(resParams.data.marker?.name);
-    setFormServer(resParams.data.marker?.server);
+    setMarkerId(resParams.data.marker.id);
+    reset({
+      name: resParams.data.marker.name,
+      description: resParams.data.marker.description,
+      server: resParams.data.marker.server,
+      type: resParams.data.marker.id_type,
+      cordX: resParams.data.marker.x,
+      cordZ: resParams.data.marker.z,
+      id: resParams.data.marker.id,
+    });
   }
 
   return (
     <div className={styles["box_map_add_wrapper"]}>
       <div className={styles["columns_add_one"]}>
-        <div className={styles["row_wrapper_content"]}>
-          <BackButton
-            onClick={() => {
-              navigate(-1);
-            }}
-          />
-          <FormTitle title="Имя:" min={MIN_NAME} max={MAX_NAME} length={formName.length} />
-          <Input
-            defaultValue={formName}
-            onChange={(e) => {
-              setFormName(e.target.value);
-              checkName(e.target.value, MIN_NAME, MAX_NAME, setErrorName);
-            }}
-          />
-          <FormTitle title="Описание:" min={MIN_DESCRIPTION} max={MAX_DESCRIPTION} length={formDescription.length} />
-          <Textarea
-            defaultValue={formDescription}
-            onChange={(e) => {
-              setFormDescription(e.target.value);
-              checkDescription(e.target.value, MIN_DESCRIPTION, MAX_DESCRIPTION, setErrorDescription);
-            }}
-            rows="4"
-          />
-        </div>
-        <div className={styles["row_wrapper_content"]}>
-          <FormTitle title="Сервер:" count={false} />
-          <Select list={WORLD_VALUE} onChange={(e) => setFormServer(e.target.value)} defaultValue={formServer} />
-          <FormTitle title="Тип метки:" count={false} />
-          <Select list={VALUE_OPTION} onChange={(e) => setFormType(e.target.value)} defaultValue={formType} />
-        </div>
-        <div className={styles["coordinates_wrapper"]}>
-          <FormTitle title="Координаты:" count={false} required={false} />
-          <div className={styles["block_row"]}>
-            <div className={styles["row_wrapper_content_custom"]}>
-              <FormTitle title="X:" count={false} />
-              <Input
-                defaultValue={formX}
-                onChange={(e) => {
-                  setFormX(e.target.value);
-                  checkCoordinates(e.target.value, setErrorCoordinates);
-                }}
-              />
-            </div>
-            <div className={styles["row_wrapper_content_custom"]}>
-              <FormTitle title="Z:" count={false} />
-              <Input
-                defaultValue={formZ}
-                onChange={(e) => {
-                  setFormZ(e.target.value);
-                  checkCoordinates(e.target.value, setErrorCoordinates);
-                }}
-              />
-            </div>
+        <form>
+          <div className={styles["row_wrapper_content"]}>
+            <BackButton onClick={() => navigate(-1)} />
+            <FormTitle title="Имя:" min={12} max={255} length={watch("name")?.length || 0} />
+            <Input type="text" autoComplete="off" placeholder="&nbsp;" {...formFields["name"]} />
+            <ErrorRender errors={errors} name="name" />
+            <FormTitle title="Описание:" min={24} max={255} length={watch("description")?.length || 0} />
+            <Textarea rows="4" max_height="medium" {...formFields["description"]} />
+            <ErrorRender errors={errors} name="description" />
           </div>
-          <Notifications inf="Учтите, что визуально точки смещаются примерно на 30-50 блоков вниз!" type="warn" />
-        </div>
+          <div className={styles["row_wrapper_content"]}>
+            <FormTitle title="Сервер:" count={false} />
+            <Select list={WORLD_VALUE} {...formFields["server"]} />
+            <FormTitle title="Тип метки:" count={false} />
+            <Select list={OPTION_VALUE} {...formFields["type"]} />
+          </div>
+          <div className={styles["coordinates_wrapper"]}>
+            <FormTitle title="Координаты:" count={false} required={false} />
+            <div className={styles["block_row"]}>
+              <div className={styles["row_wrapper_content_custom"]}>
+                <FormTitle title="X:" count={false} />
+                <Input type="number" {...formFields["cordX"]} />
+              </div>
+              <div className={styles["row_wrapper_content_custom"]}>
+                <FormTitle title="Z:" count={false} />
+                <Input type="number" {...formFields["cordZ"]} />
+              </div>
+            </div>
+            <Notifications inf="Учтите, что визуально точки смещаются примерно на 30-50 блоков вниз!" type="warn" />
+          </div>
+        </form>
         <div className={styles["actions_box"]}>
-          <Button view="submit" label="Сохранить" onClick={id === "new" ? addMarker : saveMarker} />
-          {id === "new" ? null : (
-            <Button
-              view="delete"
-              label="Удалить"
-              onClick={() => {
-                setIsConfirmActive(true);
-              }}
-            />
-          )}
+          <Button
+            view="submit"
+            label="Сохранить"
+            disabled={!isValid}
+            onClick={markerId === -1 ? handleSubmit(addMarker) : handleSubmit(editMarker)}
+          />
+          {markerId === -1 ? null : <Button view="delete" label="Удалить" onClick={() => setIsConfirmActive(true)} />}
           <ConfirmModal
             open={isConfirmActive}
             close={() => setIsConfirmActive(false)}
             no={() => setIsConfirmActive(false)}
-            yes={() => deleteMarker()}
+            yes={handleSubmit(deleteMarker)}
           />
         </div>
       </div>
       <div className={styles["columns_add_two"]}>
-        {id === "new" ? (
+        {markerId === -1 ? (
           <iframe title="map" src="https://map.gmgame.ru/#/-7/64/-54/-4/GMGameWorld/over" width="100%" height="100%" />
         ) : (
           <iframe
